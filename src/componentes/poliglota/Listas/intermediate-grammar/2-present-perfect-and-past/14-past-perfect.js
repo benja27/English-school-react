63638faab6e5a22072aba5542b7c3c68798f1be8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-10 20:26:08 -0600

    Content: Add past perfect sentences

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/14-past-perfect.js
@@ -0,0 +1,185 @@
+//14 Past Perfect (I had done)
+
+const pastPerfect = [
+    // Conversation 1
+    [
+      "Sara llegó a la fiesta.",
+      "Sara got to the party."
+    ],
+  
+    // Conversation 2
+    [
+      "Cuando Sarah llegó a la fiesta, Eric ya se había ido a casa.",
+      "When Sarah arrived at the party, Eric had already gone home."
+    ],
+  
+    // Conversation 3
+    [
+      "Cuando llegamos a casa anoche, descubrimos que alguien había entrado a nuestra casa.",
+      "When we got home last night, we found that somebody had broken into our house."
+    ],
+  
+    // Conversation 4
+    [
+      "Karen no quería ir al cine con nosotros porque ya había visto la película.",
+      "Karen didn’t want to go to the movies with us because she’d already seen the film."
+    ],
+  
+    // Conversation 5
+    [
+      "Al principio pensé que había hecho lo correcto. Pero pronto me di cuenta de que había cometido un gran error.",
+      "At first, I thought I’d done the right thing. But I soon realized that I’d made a big mistake."
+    ],
+  
+    // Conversation 6
+    [
+      "El hombre que estaba sentado a mi lado en el avión estaba muy nervioso. No había volado antes.",
+      "The man sitting next to me on the plane was very nervous. He hadn’t flown before."
+    ],
+  
+    // Conversation 7
+    [
+      "El hombre que estaba sentado a mi lado en el avión estaba muy nervioso. No había volado antes.",
+      "The man sitting next to me on the plane was very nervous. He had never flown before."
+    ],
+  
+    // Conversation 8
+    [
+      "¿Quién es esa mujer? Nunca la había visto antes.",
+      "Who is that woman? I’ve never seen her before."
+    ],
+  
+    // Conversation 9
+    [
+      "No teníamos hambre. Acabábamos de almorzar.",
+      "We aren’t hungry. We’ve just had lunch."
+    ],
+  
+    // Conversation 10
+    [
+      "No teníamos hambre. Acabábamos de almorzar.",
+      "We weren’t hungry. We’d just had lunch."
+    ],
+  
+    // Conversation 11
+    [
+      "La casa está sucia. No la han limpiado durante semanas.",
+      "The house is dirty. They haven’t cleaned it for weeks."
+    ],
+  
+    // Conversation 12
+    [
+      "La casa estaba sucia. No la habían limpiado durante semanas.",
+      "The house was dirty. They hadn’t cleaned it for weeks."
+    ],
+  
+    // Conversation 13
+    [
+      "¿Estaba Tom allí cuando llegaste? – Sí, pero se fue un poco después.",
+      "Was Tom there when you arrived? – Yes, but he left a little later."
+    ],
+  
+    // Conversation 14
+    [
+      "¿Estaba Tom allí cuando llegaste? – No, ya se había ido.",
+      "Was Tom there when you arrived? – No, he had already left."
+    ],
+  
+    // Conversation 15
+    [
+      "Amy no estaba en casa cuando llamé. Estaba en la casa de su madre.",
+      "Amy wasn’t at home when I called. She was at her mother’s house."
+    ],
+  
+    // Conversation 16
+    [
+      "Fuiste a la casa de Jill, pero ella no estaba allí. Había salido.",
+      "You went to Jill’s house, but she wasn’t there. She had gone out."
+    ],
+  
+    // Conversation 17
+    [
+      "Regresaste a tu ciudad natal después de muchos años. No era lo mismo que antes. Había cambiado mucho.",
+      "You went back to your hometown after many years. It wasn’t the same as before. It had changed a lot."
+    ],
+  
+    // Conversation 18
+    [
+      "Invité a Rachel a la fiesta, pero no pudo venir. Había hecho planes para hacer algo más.",
+      "I invited Rachel to the party, but she couldn’t come. She had made plans to do something else."
+    ],
+  
+    // Conversation 19
+    [
+      "Fuiste al cine anoche. Llegaste tarde. La película ya había comenzado.",
+      "You went to the movies last night. You got there late. The movie had already begun."
+    ],
+  
+    // Conversation 20
+    [
+      "Fue agradable volver a ver a Daniel después de tanto tiempo. No lo había visto en cinco años.",
+      "It was nice to see Daniel again after such a long time. I hadn't seen him in five years."
+    ],
+  
+    // Conversation 21
+    [
+      "Le ofrecí algo de comer a Sue, pero no tenía hambre. Acababa de desayunar.",
+      "I offered Sue something to eat, but she wasn’t hungry. She had just had breakfast."
+    ],
+  
+    // Conversation 22
+    [
+      "El hombre que estaba sentado a tu lado en el avión estaba muy nervioso. Fue su primer vuelo. (volar) Nunca había volado antes.",
+      "The man sitting next to you on the plane was very nervous. It was his first flight. (fly) He had never flown before."
+    ],
+  
+    // Conversation 23
+    [
+      "Una mujer entró en la habitación. Era una completa desconocida para mí. (ver) Nunca la había visto antes.",
+      "A woman walked into the room. She was a complete stranger to me. (see) I had never seen her before."
+    ],
+  
+    // Conversation 24
+    [
+      "Sam jugó al tenis ayer. No fue muy bueno en eso porque era su primer juego. (jugar) Nunca había jugado antes.",
+      "Sam played tennis yesterday. He wasn’t very good at it because it was his first game. (play) He had never played before."
+    ],
+  
+    // Conversation 25
+    [
+      "El año pasado fuimos a México, fue nuestra primera vez allí. (estar allí) Nunca habíamos estado allí antes.",
+      "Last year we went to Mexico, It was our first time there. (be there) We had never been there before."
+    ],
+  
+    // Conversation 26
+    [
+      "¿Ben estaba en la fiesta cuando llegaste? – No, se había ido (irse) a casa.",
+      "Was Ben at the party when you got there? – No, he had gone (go) home."
+    ],
+  
+    // Conversation 27
+    [
+      "Me sentí muy cansado cuando llegué a casa, así que me fui (irse) directamente a la cama.",
+      "I felt very tired when I got home, so I went (go) straight to bed."
+    ],
+  
+    // Conversation 28
+    [
+      "La casa estaba muy tranquila cuando llegué a casa. Todos se habían ido (irse) a la cama.",
+      "The house was very quiet when I got home. Everybody had gone (go) to bed."
+    ],
+  
+    // Conversation 29
+    [
+      "Lo siento, llego tarde. Mi coche se rompió (romperse) en el camino aquí.",
+      "Sorry I’m late. My car broke (break) down on the way here."
+    ],
+  
+    // Conversation 30
+    [
+      "Estábamos conduciendo por la carretera cuando vimos (ver) un coche que se había descompuesto (descomponerse), así que nos detuvimos (detenernos) para ayudar.",
+      "We were driving on the highway when we saw (see) a car that had broken (break) down, so we stopped (stop) to help."
+    ]
+  ];
+  
+  export default pastPerfect;
