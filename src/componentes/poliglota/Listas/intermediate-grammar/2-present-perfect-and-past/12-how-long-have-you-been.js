0d0b8d0e047c15bd1191a0876b8f31e03018fdb7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-10 20:03:44 -0600

    Content: create 12-how-long file, add chapter examples

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/12-how-long-have-you-been.js
@@ -0,0 +1,130 @@
+//12 How long have you (been) … ?
+
+const chapterExamples = [
+    // Conversation 1
+    [
+      "Han estado casados durante 20 años.",
+      "They have been married for 20 years."
+    ],
+    [
+      "¿Cuánto tiempo llevan casados?",
+      "How long have they been married?"
+    ],
+  
+    // Conversation 2
+    [
+      "Él ha estado en el hospital desde el lunes.",
+      "He has been in the hospital since Monday."
+    ],
+  
+    // Conversation 3
+    [
+      "¿Se conocen bien?",
+      "Do you know each other well?"
+    ],
+    [
+      "¿Se conocen desde hace mucho tiempo?",
+      "Have you known each other for a long time?"
+    ],
+  
+    // Conversation 4
+    [
+      "Ella está esperando a alguien.",
+      "She’s waiting for somebody."
+    ],
+    [
+      "Ha estado esperando toda la mañana.",
+      "She’s been waiting all morning."
+    ],
+  
+    // Conversation 5
+    [
+      "¿Tienen un coche?",
+      "Do they have a car?"
+    ],
+    [
+      "¿Cuánto tiempo llevan con su coche?",
+      "How long have they had their car?"
+    ],
+  
+    // Conversation 6
+    [
+      "He estado estudiando inglés durante seis meses.",
+      "I’ve been studying English for six months."
+    ],
+  
+    // Conversation 7
+    [
+      "Ha estado lloviendo desde la hora del almuerzo.",
+      "It’s been raining since lunchtime."
+    ],
+  
+    // Conversation 8
+    [
+      "Richard ha estado haciendo el mismo trabajo durante 20 años.",
+      "Richard has been doing the same job for 20 years."
+    ],
+  
+    // Conversation 9
+    [
+      "¿Cuánto tiempo llevas conduciendo? – Desde que tenía 17.",
+      "How long have you been driving? – Since I was 17."
+    ],
+  
+    // Conversation 10
+    [
+      "¿Cuánto tiempo has conocido a Emily?",
+      "How long have you known Emily?"
+    ],
+  
+    // Conversation 11
+    [
+      "He tenido dolor de estómago todo el día.",
+      "I’ve had a stomachache all day."
+    ],
+  
+    // Conversation 12
+    [
+      "John ha estado viviendo en Montreal durante mucho tiempo.",
+      "John has been living in Montreal for a long time."
+    ],
+    [
+      "John ha vivido en Montreal durante mucho tiempo.",
+      "John has lived in Montreal for a long time."
+    ],
+  
+    // Conversation 13
+    [
+      "¿Cuánto tiempo has estado trabajando aquí?",
+      "How long have you been working here?"
+    ],
+    [
+      "¿Cuánto tiempo has trabajado aquí?",
+      "How long have you worked here?"
+    ],
+  
+    // Conversation 14
+    [
+      "¿Siempre has vivido en el campo?",
+      "Have you always lived in the country?"
+    ],
+  
+    // Conversation 15
+    [
+      "No he visto a Tom desde el lunes (= el lunes fue la última vez que lo vi).",
+      "I haven’t seen Tom since Monday (= Monday was the last time I saw him)."
+    ],
+  
+    // Conversation 16
+    [
+      "Sue no ha llamado desde hace mucho tiempo. (= la última vez que llamó fue hace mucho tiempo).",
+      "Sue hasn’t called for ages. (= the last time she called was ages ago)."
+    ]
+  ];
+
+const howLongHaveYou = [
+
+]
+
+export default howLongHaveYou;
+  
