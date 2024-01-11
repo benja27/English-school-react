e5dfb46a420d78dc780441255ad16da6bbd5d245
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-10 20:51:52 -0600

    Content: Add use to do sentences chap 17

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/17-used-to.js
@@ -0,0 +1,129 @@
+//17 Used to (do)
+
+const usedToDo = [
+  [
+    'Solía jugar tenis mucho, pero ahora no juego muy a menudo.',
+    'I used to play tennis a lot, but I don’t play very often now.',
+  ],
+  [
+    'David solía gastar mucho dinero en ropa. Estos días no puede permitírselo.',
+    'David used to spend a lot of money on clothes. These days he can’t afford it.',
+  ],
+  [
+    '¿Vas al cine mucho? - Ya no, pero solía hacerlo.',
+    'Do you go to the movies much? – Not anymore, but I used to.',
+  ],
+  [
+    'Este edificio ahora es una tienda de muebles. Solía ser un cine.',
+    'This building is now a furniture store. It used to be a movie theater.',
+  ],
+  [
+    'Solía pensar que Mark era antipático, pero ahora me doy cuenta de que es una persona muy agradable.',
+    'I used to think Mark was unfriendly, but now I realize he’s a very nice person.',
+  ],
+  [
+    'Recientemente comencé a tomar café. Antes no solía gustarme.',
+    'I’ve started drinking coffee recently. I never used to like it before.',
+  ],
+  [
+    'Nicole solía tener el pelo muy largo cuando era niña.',
+    'Nicole used to have very long hair when she was a child.',
+  ],
+  [
+    'Solíamos vivir en un pueblo pequeño, pero ahora vivimos en Chicago.',
+    'We used to live in a small town, but now we live in Chicago.',
+  ],
+  [
+    'Solía haber cuatro cines en el pueblo. Ahora solo hay uno.',
+    'There used to be four movie theaters in town. Now there is only one.',
+  ],
+  [
+    '¿Solías comer muchos dulces cuando eras niño?',
+    'Did you used to eat a lot of candy when you were a child?',
+  ],
+  ['No solía gustarme.', 'I didn’t used to like him.'],
+  [
+    'Solía ver mucha televisión cuando era pequeño.',
+    'I used to watch TV a lot when I was little.',
+  ],
+  [
+    'Estaba viendo televisión cuando Mike llamó.',
+    'I was watching TV when Mike called.',
+  ],
+  ['Solía vivir solo.', 'I used to live alone.'],
+  ['Estoy acostumbrado a vivir solo.', 'I am used to living alone.'],
+  [
+    'David dejó de correr hace dos años. Solía correr tres millas al día.',
+    'David quit jogging two years ago. He used to jog three miles a day.',
+  ],
+  [
+    'Liz solía andar en motocicleta, pero el año pasado la vendió y compró un auto.',
+    'Liz used to ride a motorcycle, but last year she sold it and bought a car.',
+  ],
+  [
+    'Nos mudamos a España hace unos años. Solíamos vivir en París.',
+    'We moved to Spain a few years ago. We used to live in Paris.',
+  ],
+  [
+    'Suelo comer helado ahora, pero solía encantarme cuando era niño.',
+    'I seldom eat ice cream now, but I used to love it when I was a child.',
+  ],
+  [
+    'Tracy solía ser mi mejor amiga, pero ya no somos amigos.',
+    "Tracy used to be my best friend, but we aren't friends anymore.",
+  ],
+  [
+    'Ahora solo me lleva unos 40 minutos llegar al trabajo ahora que la nueva autopista está abierta. Solía llevar más de una hora.',
+    'It only takes me about 40 minutes to get to work now that the new highway is open. It used to take more than an hour.',
+  ],
+  [
+    'Solía haber un hotel cerca del aeropuerto, pero cerró hace mucho tiempo.',
+    'There used to be a hotel near the airport, but it closed a long time ago.',
+  ],
+  [
+    'Cuando vivías en Nueva York, ¿solías ir al teatro muy a menudo?',
+    'When you lived in New York, did you used to go to the theater very often?',
+  ],
+  [
+    'Ella solía viajar mucho, pero ya no hace muchos viajes estos días.',
+    'She used to travel a lot, but she doesn’t take many trips these days.',
+  ],
+  [
+    'Solía tener muchos amigos, pero ya no ve a mucha gente estos días.',
+    "She used to have lots of friends, but she doesn't see many people these days.",
+  ],
+  [
+    'Solía ser muy perezosa, pero ahora trabaja muy duro.',
+    'She used to be very lazy, but she works very hard these days.',
+  ],
+  [
+    'Antes no solía gustarle el queso, pero ahora come mucho queso.',
+    'She used to not like cheese, but she eats lots of cheese now.',
+  ],
+  [
+    'Solía ir a muchas fiestas, pero no va a una fiesta desde hace mucho tiempo.',
+    "She used to go to a lot of parties, but she hasn't been to a party for ages.",
+  ],
+  [
+    'Solía ser recepcionista de hotel, pero ahora trabaja en una librería.',
+    'She used to be a hotel desk clerk, but she works in a bookstore now.',
+  ],
+  [
+    'Solía tocar el piano, pero no toca el piano desde hace años.',
+    "She used to play the piano, but she hasn't played the piano for years.",
+  ],
+  [
+    'Antes no solía leer periódicos, pero ahora lee un periódico todos los días.',
+    'She used to never read newspapers, but she reads a newspaper every day now.',
+  ],
+  [
+    'Antes no solía beber té, ¡pero el té es genial! Le gusta ahora.',
+    'She used to not drink tea, but tea is great! She likes it now.',
+  ],
+  [
+    'Solía tener un perro, pero su perro murió hace dos años.',
+    'She used to have a dog, but her dog died two years ago.',
+  ],
+];
+
+export default usedToDo;
