7ba107e938a456114957c33ddc6a116912df2cab
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-11 10:34:42 -0600

    Content: Add conversation 18.3

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/3-future/18-present-tense-imdoing-ido.js
@@ -0,0 +1,56 @@
+// 18 Present Tenses (I amd doing / I do) with a Future Meaning
+
+const chapterSolution2 = [
+    "Ben: Can you come over on Monday night? You: Sorry, but I’m playing volleyball. Ben: What about Tuesday night then? You: No, not Tuesday. I'll be working late until 9 pm.Ben: And Wednesday night?You: I have plans to go to the theater. Ben: Well, are you free on Thursday?You: I’m afraid not. I have a meeting with Julia at 8 PM.",
+    "Ben: ¿Puedes venir el lunes por la noche? Tú: Lo siento, pero estoy jugando voleibol. Ben: ¿Qué pasa entonces con el martes por la noche? Tú: No, el martes no. Estaré trabajando hasta las 9 pm. Ben: ¿Y el miércoles por la noche? Tú: Tengo planes de ir al teatro. Ben: Bueno, ¿estás libre el jueves? Tú: Me temo que no. Tengo una reunión con Julia a las 8 de la noche."
+]
+
+const presentTenseImDoing = [
+    ...chapterSolution2,
+    ["Él está jugando tenis el lunes por la tarde.", "He is playing tennis on Monday afternoon."],
+    ["Él va al dentista el martes por la mañana.", "He is going to the dentist on Tuesday morning."],
+    ["Él va a cenar con Ann el viernes.", "He is having dinner with Ann on Friday."],
+    ["¿Qué estás haciendo el sábado por la noche? – Voy al teatro.", "What are you doing Saturday night? – I’m going to the theater."],
+    ["¿A qué hora llega Cathy mañana? – A las 10:30. La voy a encontrar en el aeropuerto.", "What time is Cathy arriving tomorrow? – At 10:30. I’m meeting her at the airport."],
+    ["No trabajo mañana, así que podemos salir a algún lugar.", "I’m not working tomorrow, so we can go out somewhere."],
+    ["Sam no juega al fútbol el próximo sábado. Se lastimó la pierna.", "Sam isn’t playing football next Saturday. He hurt his leg."],
+    ["¿Qué vas a hacer el sábado por la noche?", "What are you going to do Saturday night?"],
+    ["¿Qué estás haciendo esta noche?", "What are you doing tonight?"],
+    ["Eric se casa el próximo mes.", "Eric is getting married next month."],
+    ["Estoy cansado. Me voy a la cama ahora. Buenas noches.", "I’m tired. I’m going to bed now. Goodnight."],
+    ["Tina, ¿ya estás lista? – Sí, voy.", "Tina, are you ready yet? – Yes, I’m coming."],
+    ["Mi vuelo sale a las 11:30, así que necesito llegar al aeropuerto a las 10:00.", "My flight leaves at 11:30, so I need to get to the airport by 10:00."],
+    ["¿A qué hora comienza la película?", "What time does the movie begin?"],
+    ["Mañana es miércoles.", "It’s Wednesday tomorrow."],
+    ["Mañana es miércoles.", "Tomorrow is Wednesday."],
+    ["Empiezo mi nuevo trabajo el lunes.", "I start my new job on Monday."],
+    ["¿A qué hora terminas el trabajo mañana?", "What time do you finish work tomorrow?"],
+    ["¿A qué hora te encuentras con Ann mañana?", "What time are you meeting Ann tomorrow?"],
+    ["¿A qué hora llegas?", "What time are you arriving?"],
+    ["¿A qué hora llega el avión?", "What time does the plane arrive?"],
+    ["Voy al cine esta noche.", "I’m going to the movies tonight."],
+    ["La película comienza a las 8:15 (esta noche).", "The movie starts at 8:15 (tonight)."],
+    ["¿Adónde vas? – Quebec.", "Where are you going? – Quebec."],
+    ["¿Cuánto tiempo planeas quedarte? – Diez días.", "How long are you planning to stay? – Ten days."],
+    ["¿Cuándo planeas irte? – El próximo viernes.", "When are you planning to leave? – Next Friday."],
+    ["¿Planeas ir solo? – No, con un amigo.", "Are you planning to go alone? – No, with a friend."],
+    ["¿Planeas viajar en auto? – No, en avión.", "Are you planning to travel by car? – No, by plane."],
+    ["¿Dónde planeas quedarte? – En un hotel.", "Where are you planning to stay? – In a hotel."],
+    ["Voy al cine esta noche.", "I’m going to the movie tonight."],
+    ["¿La película comienza a las 3:30 o a las 4:30?", "Does the movie begin at 3:30 or 4:30?"],
+    ["Vamos a tener una fiesta el próximo sábado. ¿Te gustaría venir?", "We are having a party next Saturday. Would you like to come?"],
+    ["La exposición de arte se inaugura el 3 de mayo.", "The art exhibit opens on May 3."],
+    ["No salgo esta noche. Me quedo en casa.", "I am not going out tonight. I am staying at home."],
+    ["¿Estás haciendo algo mañana por la mañana? – No, estoy libre. ¿Por qué?", "Are you doing anything tomorrow morning? – No, I’m free. Why?"],
+    ["Vamos a un concierto esta noche. Comienza a las 7:30.", "We are going to a concert tonight. It starts at 7:30."],
+    ["Me voy ahora. Vine a despedirme.", "I am leaving now. I came to say goodbye."],
+    ["¿Has visto a Liz últimamente? – No, pero nos encontraremos para almorzar la próxima semana.", "Have you seen Liz recently? – No, but we are meeting for lunch next week."],
+    ["Disculpe. ¿A qué hora llega este tren a Boston?", "Excuse me. What time does this train get to Boston?"],
+    ["Julie, voy a la tienda ahora. ¿Vienes conmigo?", "Julie, I am going to the store now. Are you coming with me?"],
+    ["Me aburro con este espectáculo. ¿A qué hora termina?", "I’m bored with this show. What time does it end?"],
+    ["No voy a usar el coche esta noche, así que puedes tomarlo.", "I am not using the car tonight, so you can have it."],
+    ["Sue viene a vernos mañana. Viene volando desde Seattle, y su avión llega a las 10:15 a. m.", "Sue is coming to see us tomorrow. She is flying from Seattle, and her plane arrives at 10:15 a.m."]
+  ];
+
+  export default presentTenseImDoing;
+  
