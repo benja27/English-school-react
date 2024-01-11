f9ed71056cecbe65048972b7c1e7dac0240b9a87
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-10 20:08:14 -0600

    Content: add solution 12.1

--- src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/12-how-long-have-you-been.js
+++ src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/12-how-long-have-you-been.js
@@ -120,9 +120,70 @@ const chapterExamples = [
       "Sue no ha llamado desde hace mucho tiempo. (= la última vez que llamó fue hace mucho tiempo).",
       "Sue hasn’t called for ages. (= the last time she called was ages ago)."
     ]
-  ];
+];
+
+const chapterSolution1 = [
+    // Conversation 1
+    [
+      "Bob es un amigo mío. Lo conozco muy bien.",
+      "Bob is a friend of mine. I know him very well."
+    ],
+    [
+      "Bob es un amigo mío. Lo he conocido durante mucho tiempo.",
+      "Bob is a friend of mine. I've known him for a long time."
+    ],
+  
+    // Conversation 2
+    [
+      "Sue y Scott están casados desde julio.",
+      "Sue and Scott have been married since July."
+    ],
+  
+    // Conversation 3
+    [
+      "El clima es terrible. Está lloviendo otra vez.",
+      "The weather is awful. It’s raining again."
+    ],
+    [
+      "El clima es terrible. Ha estado lloviendo todo el día.",
+      "The weather is awful. It’s been raining all day."
+    ],
+  
+    // Conversation 4
+    [
+      "Me gusta tu casa. ¿Cuánto tiempo llevas viviendo allí?",
+      "I like your house. How long have you been living there?"
+    ],
+  
+    // Conversation 5
+    [
+      "Gary ha estado trabajando en una tienda durante los últimos meses.",
+      "Gary has been working in a store for the last few months."
+    ],
+  
+    // Conversation 6
+    [
+      "No conozco bien a Tim. Solo nos hemos encontrado algunas veces.",
+      "I don’t know Tim well. We’ve only met a few times."
+    ],
+  
+    // Conversation 7
+    [
+      "Dejé de beber café. No lo he estado bebiendo durante un año.",
+      "I quit drinking coffee. I haven’t been drinking it for a year."
+    ],
+  
+    // Conversation 8
+    [
+      "Esa es una bicicleta muy antigua. ¿Cuánto tiempo la has tenido?",
+      "That’s a very old bike. How long have you had it?"
+    ]
+];
+  
 
 const howLongHaveYou = [
+    ...chapterExamples,
+
 
 ]
 
