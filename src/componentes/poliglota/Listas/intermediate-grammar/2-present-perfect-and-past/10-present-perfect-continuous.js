8e366bb77a23407ee0a03133acd07d1b212eb03a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-10 18:51:15 -0600

    Content: Add solution 10.2

--- src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/10-present-perfect-continuous.js
+++ src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/10-present-perfect-continuous.js
@@ -1,22 +1,76 @@
 //10 Present Perfect Continuous ( I have been doing)
 const chapterExamples = [
-    ["Te falta el aliento. ¿Has estado corriendo?", "You’re out of breath. Have you been running? (= you’re out of breath now)"],
-    ["Jason está muy cansado. Ha estado trabajando muy duro.", "Jason is very tired. He’s been working very hard. (= he’s tired now)"],
-    ["¿Por qué están tan sucias tus ropas? ¿Qué has estado haciendo?", "Why are your clothes so dirty? What have you been doing?"],
-    ["(en el teléfono) Me alegra que hayas llamado. He estado pensando en llamarte...", "(on the phone) I’m glad you called. I’ve been thinking about calling you…"],
-    ["¿Dónde has estado? He estado buscándote por todas partes.", "Where have you been? I’ve been looking everywhere for you."],
-    ["¿Cuánto tiempo has estado estudiando inglés? (= aún estás estudiando inglés)", "How long have you been studying English? (= you’re still studying English)"],
-    ["Tim todavía está viendo televisión. Ha estado viendo televisión todo el día.", "Tim is still watching television. He’s been watching television all day."],
-    ["¿Dónde has estado? Te he estado buscando durante la última media hora.", "Where have you been? I’ve been looking for you for the last half hour."],
-    ["Christopher no se ha sentido bien últimamente.", "Christopher hasn’t been feeling well recently."],
-    ["Debbie es una muy buena jugadora de tenis. Ha estado jugando desde los ocho años.", "Debbie is a very good tennis player. She’s been playing since she was eight."],
-    ["Cada mañana se encuentran en el mismo café. Han estado yendo allí durante años.", "Every morning they meet in the same cafe. They’ve been going there for years."]
+  [
+    'Te falta el aliento. ¿Has estado corriendo?',
+    'You’re out of breath. Have you been running? (= you’re out of breath now)',
+  ],
+  [
+    'Jason está muy cansado. Ha estado trabajando muy duro.',
+    'Jason is very tired. He’s been working very hard. (= he’s tired now)',
+  ],
+  [
+    '¿Por qué están tan sucias tus ropas? ¿Qué has estado haciendo?',
+    'Why are your clothes so dirty? What have you been doing?',
+  ],
+  [
+    '(en el teléfono) Me alegra que hayas llamado. He estado pensando en llamarte...',
+    '(on the phone) I’m glad you called. I’ve been thinking about calling you…',
+  ],
+  [
+    '¿Dónde has estado? He estado buscándote por todas partes.',
+    'Where have you been? I’ve been looking everywhere for you.',
+  ],
+  [
+    '¿Cuánto tiempo has estado estudiando inglés? (= aún estás estudiando inglés)',
+    'How long have you been studying English? (= you’re still studying English)',
+  ],
+  [
+    'Tim todavía está viendo televisión. Ha estado viendo televisión todo el día.',
+    'Tim is still watching television. He’s been watching television all day.',
+  ],
+  [
+    '¿Dónde has estado? Te he estado buscando durante la última media hora.',
+    'Where have you been? I’ve been looking for you for the last half hour.',
+  ],
+  [
+    'Christopher no se ha sentido bien últimamente.',
+    'Christopher hasn’t been feeling well recently.',
+  ],
+  [
+    'Debbie es una muy buena jugadora de tenis. Ha estado jugando desde los ocho años.',
+    'Debbie is a very good tennis player. She’s been playing since she was eight.',
+  ],
+  [
+    'Cada mañana se encuentran en el mismo café. Han estado yendo allí durante años.',
+    'Every morning they meet in the same cafe. They’ve been going there for years.',
+  ],
 ];
 
 const chapterSolution1 = [
-    ["Han estado de compras.", "They’ve been shopping."],
-    ["Ella ha estado viendo televisión.", "She has been watching TV."],
-    ["Han estado jugando tenis.", "They’ve been playing tennis."],
-    ["Él ha estado corriendo.", "He has been running."]
+  ['Han estado de compras.', 'They’ve been shopping.'],
+  ['Ella ha estado viendo televisión.', 'She has been watching TV.'],
+  ['Han estado jugando tenis.', 'They’ve been playing tennis.'],
+  ['Él ha estado corriendo.', 'He has been running.'],
 ];
 
+const chapterSolution2 = [
+  [
+    'Te encuentras con Paul mientras él sale de la piscina. Preguntas: ¿Has estado nadando?',
+    'You meet Paul as he is leaving the swimming pool. You ask: Have you been swimming?',
+  ],
+
+  [
+    'Acabas de llegar para encontrarte con un amigo que te está esperando. Preguntas: ¿Has estado esperando mucho?',
+    'You have just arrived to meet a friend who is waiting for you. You ask: Have you been waiting long?',
+  ],
+
+  [
+    'Te encuentras con un amigo en la tienda. Su cara y manos están muy sucias. Preguntas: ¿Qué has estado haciendo?',
+    'You meet a friend at the store. His face and hands are very dirty. You ask: What have you been doing?',
+  ],
+
+  [
+    'Un amigo tuyo ahora trabaja en un gimnasio. Quieres saber cuánto tiempo lleva. Preguntas: Cuanto timepo has estado trabajando ahi',
+    'A friend of yours is now working at a gym. You want to know how long. You ask: How long have you been working there?',
+  ],
+];
