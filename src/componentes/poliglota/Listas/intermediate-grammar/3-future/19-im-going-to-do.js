a196d040c00fc4889d65b89f939a2b630bda5f5d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-11 11:18:51 -0600

    Add Sentences I'm going to do

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/3-future/19-im-going-to-do.js
@@ -0,0 +1,40 @@
+// 19 ( I’m) going to (do)
+
+const imGoingToDo = [
+    [
+      "¿Vas a ver el partido de fútbol en la televisión esta noche? – No, voy a ir a la cama temprano. Estoy cansado de mi viaje.",
+      "Are you going to watch the football game on TV tonight? – No, I’m going to go to bed early. I’m tired from my trip."
+    ],
+    [
+      "Escuché que Lisa ganó algo de dinero. ¿Qué va a hacer con eso? – Va a comprar un coche nuevo.",
+      "I heard Lisa won some money. What is she going to do with it? – She’s going to buy a new car."
+    ],
+    [
+      "Voy a hacer una llamada rápida. ¿Puedes esperarme?",
+      "I’m going to make a quick phone call. Can you wait for me?"
+    ],
+    ["Este queso huele horrible. No lo voy a comer.", "This cheese smells awful. I’m not going to eat it."],
+    ["¿A qué hora te encuentras con Amanda esta noche?", "What time are you meeting Amanda tonight?"],
+    ["Me voy mañana. Ya tengo mi boleto de avión.", "I’m leaving tomorrow. I already have my plane ticket."],
+    ["Las ventanas están sucias. – Sí, lo sé. Voy a lavarlas más tarde.", "The windows are dirty. – Yes, I know. I’m going to wash them later."],
+    [
+      "He decidido no quedarme aquí por más tiempo. Mañana voy a buscar otro lugar para vivir.",
+      "I’ve decided not to stay here any longer. Tomorrow I’m going to look for another place to live."
+    ],
+    ["¡Mira esas nubes oscuras! Va a llover.", "Look at those dark clouds! It’s going to rain."],
+    ["Me siento fatal. Creo que voy a enfermarme.", "I feel awful. I think I’m going to be sick."],
+    [
+      "La situación económica es mala ahora, y las cosas van a empeorar.",
+      "The economic situation is bad now, and things are going to get worse."
+    ],
+    ["Íbamos a volar a Nueva York, pero luego decidimos conducir en su lugar.", "We were going to fly to New York, but then we decided to drive instead."],
+    ["Peter iba a tomar el examen, pero cambió de opinión.", "Peter was going to take the exam, but he changed his mind."],
+    [
+      "Estaba a punto de cruzar la calle cuando alguien gritó, ‘’¡Detente!’’.",
+      "I was just going to cross the street when somebody shouted, ‘’Stop!’’."
+    ],
+    ["Pensé que iba a llover, pero no lo hizo.", "I thought it was going to rain, but it didn’t."]
+  ];
+  
+
+export default imGoingToDo;
