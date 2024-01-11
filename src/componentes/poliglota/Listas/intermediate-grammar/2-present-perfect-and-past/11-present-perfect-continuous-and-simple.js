8c339734455195ed4b20c2172f5b07b99e232429
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-10 19:01:07 -0600

    Content: Add chapter 11 examples

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/2-present-perfect-and-past/11-present-perfect-continuous-and-simple.js
@@ -0,0 +1,75 @@
+// 11 Present Perfect Continous and Simple (I have been doing and I have done)
+
+const chapterExamples = [
+  ['Ella ha estado pintando el techo.', 'She has been painting the ceiling.'],
+
+  ['Ella ha pintado el techo.', 'She has painted the ceiling.'],
+
+  [
+    'Mis manos están muy sucias. He estado arreglando el coche.',
+    'My hands are very dirty. I’ve been fixing the car.',
+  ],
+
+  ['He arreglado el coche.', 'I’ve fixed the car.'],
+
+  [
+    'Joe ha estado comiendo demasiado últimamente. Debería comer menos.',
+    'Joe has been eating too much recently. He should eat less.',
+  ],
+
+  [
+    'Alguien se ha comido todos mis caramelos. La caja está vacía.',
+    'Somebody has eaten all my candy. The box is empty.',
+  ],
+
+  [
+    'Es agradable volver a verte. ¿Qué has estado haciendo desde la última vez que te vimos?',
+    'It’s nice to see you again. What have you been doing since the last time we saw you?',
+  ],
+
+  [
+    '¿Dónde está el libro que te di? ¿Qué has hecho con él?',
+    'Where’s the book I gave you? What have you done with it?',
+  ],
+
+  [
+    '¿Dónde has estado? ¿Has estado jugando al tenis?',
+    'Whare have you been? Have you been playing tennis?',
+  ],
+
+  ['¿Alguna vez has jugado tenis?', 'Have you ever played tennis?'],
+
+  [
+    '¿Cuánto tiempo has estado leyendo ese libro?',
+    'How long have you been reading that book?',
+  ],
+
+  ['¿Cuánto de ese libro has leído?', 'How much of that book have you read?'],
+
+  [
+    'Lisa todavía está escribiendo su informe. Lo ha estado escribiendo todo el día.',
+    'Lisa is still writing her report. She’s been writing it all day.',
+  ],
+
+  ['Lisa ha escrito 10 páginas hoy.', 'Lisa has written 10 pages today.'],
+
+  [
+    'Han estado jugando al tenis desde las 2:00.',
+    'The’ve been playin tennis since 2:00.',
+  ],
+
+  [
+    'Han jugado tenis tres veces esta semana.',
+    'The’ve played tennis three times this week.',
+  ],
+
+  [
+    'Estoy estudiando español, pero no lo he estado estudiando por mucho tiempo.',
+    'I’m studying Spanish, but I haven’t been studying it very long.',
+  ],
+
+  [
+    'Estoy estudiando español, pero aún no he aprendido mucho.',
+    'I’m studying Spanish, but I haven’t learned very much yet.',
+  ],
+];
