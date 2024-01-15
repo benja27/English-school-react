320cbcb7a39cf40022bc340ec96464edfe4ac227
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-14 19:50:43 -0600

    Content: Add chapter 23 will be doing and will have done

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/3-future/23-will-be-doing-and-will-have-done.js
@@ -0,0 +1,33 @@
+// 23 Will be doing and Will have done
+
+const willBeDoingAndWillHaveDone = [
+    ['Una hora a partir de ahora, el estadio estará lleno. Todos estarán viendo el juego.', 'An hour from now, the stadium will be full. Everyone will be watching the game.'],
+    ['Tres horas a partir de ahora, el estadio estará vacío. El juego habrá terminado. Todos se habrán ido a casa.', 'Three hours from now, the stadium will be empty. The game will have ended. Everyone will have gone home.'],
+    ['Me voy de vacaciones este sábado. A esta hora la próxima semana, estaré tumbado en la playa o nadando en el océano.', 'I’m leaving on vacation this Saturday. This time next week, I’ll be lying on the beach or swimming in the ocean.'],
+    ['No tienes posibilidad de conseguir el trabajo. Estarás perdiendo el tiempo si aplicas para ello.', 'You have no chance of getting the job. You’ll be wasting your time if you apply for it.'],
+    ['No me llames entre las 7 y las 8. Estaremos cenando.', 'Don’t call me between 7 and 8. We’ll be having dinner.'],
+    ['Esperemos a que María llegue, y luego cenaremos.', 'Let’s wait for Maria to arrive, and then we’ll have dinner.'],
+    ['A las 10:00 de ayer, Kelly estaba en la oficina. Estaba trabajando.', 'At 10:00 yesterday, Kelly was at the office. She was working.'],
+    ['Son las 10:00 ahora. Ella está en la oficina. Está trabajando.', 'It’s 10:00 now. She is at the office. She is working.'],
+    ['A las 10:00 de mañana, ella estará en la oficina. Estará trabajando.', 'At 10:00 tomorrow, she will be at the office. She will be working.'],
+    ['El gobierno hará una declaración sobre la crisis más tarde hoy.', 'The government will be making a statement about the crisis later today.'],
+    ['¿Te vas de vacaciones este verano?', 'Will you be going away this summer?'],
+    ['Más tarde en el programa, hablaré con el Ministro de Educación...', 'Later in the program, I’ll be talking to the Minister of Education…'],
+    ['Nuestro mejor jugador está herido y no jugará en el partido del sábado.', 'Our best player is injured and won’t be playing in the game on Saturday.'],
+    ['Sally siempre sale para el trabajo a las 8:30 de la mañana. No estará en casa a las 9:00, se habrá ido al trabajo.', 'Sally always leaves for work at 8:30 in the morning. She won’t be at home at 9:00, She’ll have gone to work.'],
+    ['Estamos tarde. La película ya habrá comenzado para cuando lleguemos al cine.', 'We’re late. The movie will already have started by the time we get to the theater.'],
+    ['Ted y Amy llevan casados 24 años. El próximo año habrán estado casados por 25 años. Cuando nació su primer hijo, llevaban casados tres años.', 'Ted and Amy have been married for 24 years. Next year they will have been married for 25 years. When their first child was born, they had been married for three years.'],
+    ['No me llames entre las 7 y las 8. Estaremos cenando entonces.', 'Don’t call me between 7 and 8. We’ll be having (we / have) dinner then.'],
+    ['Llámame después de las 8:00. Habremos terminado de cenar para entonces.', 'Call me after 8:00. We will have finished dinner by then.'],
+    ['Mañana por la tarde vamos a jugar al tenis de 3:00 a 4:30. Así que a las 4:00, estaremos jugando al tenis.', 'Tomorrow afternoon we’re going to play tennis from 3:00 until 4:30. So at 4:00, we will be playing tennis.'],
+    ['¿Podemos encontrarnos mañana por la tarde? –Lo siento, no puedo. Estaré trabajando.', 'Can we meet tomorrow afternoon? –I’m sorry, I can’t. I will be working.'],
+    ['B: tiene que ir a una reunión que comienza a las 10:00. Durará aproximadamente una hora. A: ¿Estarás libre a las 11:30? B: Sí, la reunión habrá terminado para entonces.', 'B: has to go to a meeting that begins at 10:00. It will last about an hour. A: Will you be free at 11:30? B: Yes, the meeting will have ended by then.'],
+    ['Ben está de vacaciones, y está gastando su dinero muy rápidamente. Si continúa así, habrá gastado todo su dinero antes del final de sus vacaciones.', 'Ben is on vacation, and he is spending his money very quickly. If he continues like this, he will have spent all his money before the end of his vacation.'],
+    ['¿Crees que seguirás haciendo el mismo trabajo dentro de 10 años?', 'Do you think you will still be doing the same job 10 years from now?'],
+    ['Lisa es de Nueva Zelanda. Está viajando por Sudamérica en este momento. Hasta ahora ha recorrido alrededor de 1,000 millas. Al final del viaje, habrá recorrido más de 3,000 millas.', 'Lisa is from New Zealand. She is traveling around South America right now. So far she has traveled about 1,000 miles. By the end of the trip, she will have traveled more than 3,000 miles.'],
+    ['Si necesitas contactarme, estaré hospedándome en el Hotel Bellmore hasta el viernes.', 'If you need to contact me, I will be staying at the Bellmore Hotel until Friday.'],
+    ['A: ¿Vas a ver a Laura mañana? B: Sí, probablemente. ¿Por qué? A: Le presté este CD. ¿Puedes devolvérselo?', 'A: Will you see Laura tomorrow? B: Yes, probably. Why? A: I borrowed this CD from her. Can you give it back to her?'],
+  ];
+
+  export default willBeDoingAndWillHaveDone;
+  
