00374ea6e81764b143a124d38079986468c4d73c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-14 19:45:50 -0600

    Content: Add chapter 22 I will and I'm going to

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/3-future/22-i-will-and-im-going-to.js
@@ -0,0 +1,38 @@
+// 22 I will and I’m going to 
+
+import will from "./20-will";
+
+const willAndGoingTo = [
+    ['Tengamos una fiesta. – Esa es una gran idea. Invitaremos a mucha gente.', 'Let’s have a party. – That’s a great idea. We’ll invite lots of people.'],
+    ['Sue y yo hemos decidido hacer una fiesta. Vamos a invitar a mucha gente.', 'Sue and I have decided to have a party. We’re going to invite lots of people.'],
+    ['Daniel llamó mientras no estabas. – Vale. Lo llamaré de vuelta.', 'Daniel called while you were out. – Ok. I’ll call him back.'],
+    ['Daniel llamó mientras no estabas. Sí, lo sé. Voy a llamarlo de vuelta.', 'Daniel called while you were out. Yes, I know. I’m going to call him back.'],
+    ['Anna está en el hospital. ¿Ah, sí? No lo sabía. Iré a visitarla.', 'Anna is in the hospital. Oh really? I didn’t know. I’ll go and visit her.'],
+    ['Anna está en el hospital. Sí, lo sé. Voy a visitarla esta noche.', 'Anna is in the hospital. Yes, I know. I’m going to visit her tonight.'],
+    ['Creo que el clima estará agradable más tarde.', 'I think the weather will be nice later.'],
+    ['Creo que el clima va a estar agradable más tarde.', 'I think the weather is going to be nice later.'],
+    ['Mira esas nubes negras. Va a llover.', 'Look at those black clouds. It’s going to rain.'],
+    ['Me siento terrible. Creo que voy a enfermarme.', 'I feel terrible. I think I’m going to be sick.'],
+    ['Tom probablemente llegará alrededor de las 8:00.', 'Tom will probably get here at about 8:00.'],
+    ['Creo que a Jessica le gustará el regalo que le compramos.', 'I think Jessica will like the present we bought for her.'],
+    ['Estos zapatos están muy bien hechos. Durarán mucho tiempo.', 'These shoes are very well made. They’ll last a long time.'],
+    ['¿Por qué estás encendiendo la televisión? – Voy a ver las noticias.', 'Why are you turning on the television? – I’m going to watch the news.'],
+    ['Oh, acabo de darme cuenta. No tengo dinero. – ¿No? Bueno, no te preocupes. Te prestaré algo.', 'Oh, I just realized. I don’t have any money. – You don’t? Well, don’t worry. I'll lend you some.'],
+    ['Me duele la cabeza. – ¿De verdad? Espera un segundo y te conseguiré una aspirina.', 'I have a headache. – You do? Wait a second and I'll get an aspirin for you.'],
+    ['¿Por qué estás llenando ese cubo de agua? – Voy a lavar el coche.', 'Why are you filling that bucket with water? – I'm going to wash the car.'],
+    ['He decidido pintar esta habitación. – ¿Ah, sí? ¿De qué color la vas a pintar?', 'I’ve decided to paint this room. – Oh, really? What color are you going to paint it?'],
+    ['¿A dónde vas? ¿Vas de compras? – Sí, voy a comprar algunas cosas para la cena.', 'Where are you going? Are you going shopping? – Yes, I'm going to buy some things for dinner.'],
+    ['No sé cómo usar esta cámara. – Es fácil. Te mostraré.', 'I don’t know how to use this camera. – It’s easy. I'll show you.'],
+    ['¿Enviaste esa carta por mí? – Oh, lo siento. Lo olvidé por completo. Lo haré ahora.', 'Did you mail that letter for me? – Oh, Im sorry. I completely forgot. I'll do it now.'],
+    ['El techo de esta habitación no parece muy seguro, ¿verdad? – No, parece como si fuera a caerse.', 'The ceiling in this room doesn’t look very safe, does it? – No, it looks as if it's going to fall down.'],
+    ['¿Dan ha decidido qué hacer cuando termine la secundaria? – Sí. Todo está planeado. Tomará unos meses libres. – Entonces va a empezar clases en la universidad comunitaria.', 'Has Dan decided what to do when he finishes high school? – Yes. Everything is planned. He's going to take a few months off. Then he's going to start classes at the community college.'],
+    ['Llamante: Hola, ¿puedo hablar con Jim, por favor?', 'Caller: Hello, Can I speak to Jim, please?\nYou: Just a minute. I’ll get him.'],
+    ['Es un buen día, así que he decidido dar un paseo. Justo antes de irme, le dices a tu amigo.', 'You: The weather’s too nice to stay indoors. I'm going to take a walk. Friend: Good idea. I think I'll join you.'],
+    ['Tu amiga está preocupada porque ha perdido una carta importante.', 'You: Don’t worry about the letter. I’m sure you'll find it.'],
+    ['Hubo un trabajo anunciado en el periódico recientemente. Al principio, estabas interesado, pero luego decidiste no presentar la solicitud.', 'Friend: Have you decided what to do about that job you were interested in? You: Yes, I'm not going to apply for it.'],
+    ['Tú y un amigo llegan a casa muy tarde. Otras personas en la casa están dormidas. Tu amigo hace ruido.', 'You: Shh! Don’t make so much noise. You'll wake everybody up.'],
+    ['John tiene que ir al aeropuerto para tomar un avión mañana por la mañana.', 'John: Ann, I need a ride to the airport tomorrow morning. Ann: That’s no problem. I'll take you. What time is your flight? John: 10:50. Ann: OK, we'll leave at about 8:00.'],
+    ['Más tarde ese día, Joe le ofrece a John llevarlo al aeropuerto.', 'Joe: John, do you want me to take you to the airport? John: No thanks, Joe. Ann will take me. (Ann / take)'],
+  ];
+  
+export default willAndGoingTo;
