ffc7ac540a2b4db66011b6bd90dcfdfc947f6445
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-24 19:33:12 -0600

    Content: Add Must

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/4-modals/27-must.js
@@ -0,0 +1,22 @@
+// Must (You must be tired, etc).
+
+const must = [
+    ['My house is next to the freeway. –It must be very noisy.', 'Mi casa está al lado de la autopista. - Debe ser muy ruidoso. 🏠🛣️'],
+    ['You’ve been traveling all day. You must be tired.', 'Has estado viajando todo el día. Debes estar cansado. ✈️😴'],
+    ['Jim is a hard worker. –Jim? You must be joking. He’s very lazy.', 'Jim es trabajador. - ¿Jim? Debes estar bromeando. Es muy perezoso. 💼😆'],
+    ['I’m sure Sally gave me her phone number. I must have it somewhere.', 'Estoy seguro de que Sally me dio su número de teléfono. Debo tenerlo en algún lugar. 📞🤔'],
+    ['Their car isn’t outside their house. They must not be home.', 'Su auto no está afuera de su casa. Deben no estar en casa. 🏠🚗'],
+    ["Brian said he would be here by 9:30. It's 10:00 now, and he’s never late. He must not be coming.", 'Brian dijo que estaría aquí para las 9:30. Son las 10:00 ahora, y nunca llega tarde. Debe ser que no viene. ⌚🤷'],
+    ['They haven’t lived here very long. They must not know many people.', 'No han vivido aquí mucho tiempo. Deben no conocer a mucha gente. 🏡👥'],
+    ['We used to live close to the freeway. –Did you? It must have been noisy.', 'Solíamos vivir cerca de la autopista. - ¿En serio? Debe haber sido ruidoso. 🛣️🤔'],
+    ['There’s nobody at home. They must have gone out.', 'No hay nadie en casa. Deben haber salido. 🚶‍♂️🚪'],
+    ['I’ve lost one of my gloves. I must have dropped it somewhere.', 'He perdido uno de mis guantes. Debo haberlo dejado caer en algún lugar. 🧤🤦‍♂️'],
+    ['She walked past me without speaking. She must not have seen me.', 'Pasó por mi lado sin hablar. Debe ser que no me vio. 👀🚶‍♀️'],
+    ['Tom walked into a wall. He must not have been looking where he was going.', 'Tom se chocó con una pared. Debe ser que no estaba mirando por dónde iba. 🚶‍♂️🤦‍♂️'],
+    ['How can you say such a thing? You can’t be serious!', '¿Cómo puedes decir semejante cosa? ¡No puedes estar hablando en serio! 😮🤨'],
+    ['Joe wants something to eat. –But he just had lunch. He can’t be hungry already.', 'Joe quiere algo para comer. - Pero acaba de almorzar. No puede tener hambre tan pronto. 🍔🤷'],
+    ['I offered Bill something to eat, but he didn’t want anything. –He must not be hungry.', 'Le ofrecí algo de comer a Bill, pero no quería nada. - Debe ser que no tiene hambre. 🍽️🤔'],
+  ];
+  
+
+export default must;
