e874ccb727e9ecd3ae714e30e6f3809dbf9edd2e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-25 19:48:34 -0600

    Content: Add 30 Have to and must

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/4-modals/30-have-to-and-must.js
@@ -0,0 +1,31 @@
+// 30 Have to and must
+
+const haveToAndMust = [
+    ['You can’t turn right here. You have to turn left. ⬅️', 'No puedes girar a la derecha aquí. Tienes que girar a la izquierda. ⬅️'],
+    ['I have to get up early tomorrow. My flight leaves at 7:30. ⏰', 'Tengo que levantarme temprano mañana. Mi vuelo sale a las 7:30. ⏰'],
+    ['Jason can’t meet us tonight. He has to work late. 🕰️', 'Jason no puede encontrarnos esta noche. Él tiene que trabajar hasta tarde. 🕰️'],
+    ['Last week Nicole broke her arm and had to go to the hospital. 💉', 'La semana pasada Nicole se rompió el brazo y tuvo que ir al hospital. 💉'],
+    ['Have you ever had to go to the hospital? 🏥', '¿Alguna vez has tenido que ir al hospital? 🏥'],
+    ['What do I have to do to get a driver’s license? 🚗', '¿Qué tengo que hacer para obtener una licencia de conducir? 🚗'],
+    ['Does Kimberly have to work tomorrow? 📅', '¿Kimberly tiene que trabajar mañana? 📅'],
+    ['Why did you have to leave early? ❓', '¿Por qué tuviste que salir temprano? ❓'],
+    ['I don’t have to get up early tomorrow. ⏰', 'No tengo que levantarme temprano mañana. ⏰'],
+    ['Kimberly doesn’t have to work on Saturdays. 📅', 'Kimberly no tiene que trabajar los sábados. 📅'],
+    ['We didn’t have to pay to park the car. 💰', 'No tuvimos que pagar para estacionar el auto. 💰'],
+    ['They can’t fix my computer, so I’ll have to buy a new one. 💻', 'No pueden arreglar mi computadora, así que tendré que comprar una nueva. 💻'],
+    ['They can’t fix my car, so I’m going to have to buy a new one. 🚗', 'No pueden arreglar mi coche, así que voy a tener que comprar uno nuevo. 🚗'],
+    ['I might have to leave the meeting early. ⌚', 'Podría tener que salir de la reunión temprano. ⌚'],
+    ['I may have to leave the class early. ⌚', 'Puede que tenga que salir de la clase temprano. ⌚'],
+    ['The economic situation is bad. The government must do something about it. 💼', 'La situación económica es mala. El gobierno debe hacer algo al respecto. 💼'],
+    ['The pollution is terrible. The government hast to do something about it. 😷', 'La contaminación es terrible. El gobierno debe hacer algo al respecto. 😷'],
+    ['Answer all the questions. You must write your answers in ink. ✒️', 'Responde todas las preguntas. Debes escribir tus respuestas con tinta. ✒️'],
+    ['Applications for the job must be received by May 18. 📅', 'Las solicitudes para el trabajo deben recibirse antes del 18 de mayo. 📅'],
+    ['Students must not use cellphones in class. 📵', 'Los estudiantes no deben usar teléfonos celulares en clase. 📵'],
+    ['You must keep this a secret. You must not tell anybody. 🤫', 'Debes mantener esto en secreto. No debes contarle a nadie. 🤫'],
+    ['You don’t have to tell Tim about what happened. I can tell him myself. (You don’t need to tell him, but it’s OK if you do)', 'No necesitas contarle a Tim lo que pasó. Puedo decírselo yo mismo. (No es necesario que se lo digas, pero está bien si lo haces)'],
+    ['I’ve got to work tomorrow. / I have to work tomorrow. ⏳', 'Tengo que trabajar mañana. ⏳'],
+    ['He’s got to visit his aunt tonight. / He has to visit his aunt tonight. 🌙', 'Él tiene que visitar a su tía esta noche. 🌙'],
+  ];
+  
+
+export default haveToAndMust;
