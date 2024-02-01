f88aa83b4762cad49192247b7d21c206217c7f9f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-01-31 19:02:50 -0600

    Content: add 43 it is aid that .. he is said to... he is supposed to

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/6-passive/43-it-is-said-that.js
@@ -0,0 +1,24 @@
+// 43 It is said that … He is said to … He is supposed to …
+
+const itIsSaidThat = [
+    ['Nobody knows exactly how old he is but, It is said that he is 108 years old. 🕵️‍♂️', 'Nadie sabe exactamente cuántos años tiene, pero se dice que tiene 108 años. 🕵️‍♂️'],
+    ['Cathay works very hard. It is said she works 16 hours a day. 👩‍💼', 'Cathay trabaja muy duro. Se dice que trabaja 16 horas al día. 👩‍💼'],
+    ['The police are looking for a missing boy. It is believed that the boy is wearing a white sweater and blue jeans. 👮‍♂️', 'La policía está buscando a un niño desaparecido. Se cree que el niño lleva un suéter blanco y jeans azules. 👮‍♂️'],
+    ['The strike started three weeks ago. It is expected that it will end soon. ⛔', 'La huelga comenzó hace tres semanas. Se espera que termine pronto. ⛔'],
+    ['A friend of mine has been arrested. It is alleged that he hit a police officer. 🚓', 'Un amigo mío ha sido arrestado. Se alega que golpeó a un oficial de policía. 🚓'],
+    ['The two houses belong to the same family. It is said that there is a secret tunnel between them. 🏠', 'Las dos casas pertenecen a la misma familia. Se dice que hay un túnel secreto entre ellas. 🏠'],
+    ['It is reported that two people were injured in the explosion. 📰', 'Se informa que dos personas resultaron heridas en la explosión. 📰'],
+    ['Two people are reported to have been injured in the explosion. 🚑', 'Se informa que dos personas resultaron heridas en la explosión. 🚑'],
+    ['Let’s go and see that movie. It’s supposed to be good. 🎥', 'Vamos a ver esa película. Se supone que es buena. 🎥'],
+    ['Mark is supposed to have hit a police officer, but I don’t believe it. 👊', 'Se supone que Mark golpeó a un oficial de policía, pero no lo creo. 👊'],
+    ['The plan is supposed to be a secret, but everybody seems to know about it. 🤫', 'Se supone que el plan es secreto, pero parece que todos lo saben. 🤫'],
+    ['What are you doing at work? You’re supposed to be on vacation. 🏖️', '¿Qué estás haciendo en el trabajo? Se supone que estás de vacaciones. 🏖️'],
+    ['Jane was supposed to call me last night, but she didn’t. 📞', 'Jane debía llamarme anoche, pero no lo hizo. 📞'],
+    ['Our guests were supposed to come at 7:30, but they were late. ⏰', 'Nuestros invitados debían venir a las 7:30, pero llegaron tarde. ⏰'],
+    ['I’d better hurry. I’m supposed to meet Chris in 10 minutes. ⏱️', 'Más me vale apurarme. Se supone que debo encontrarme con Chris en 10 minutos. ⏱️'],
+    ["You’re not supposed to park your car here. It’s private parking only. 🚗", "No se supone que estaciones tu auto aquí. Es solo estacionamiento privado. 🚗"],
+    ["Mr. Bruno is much better after his operation, but he’s still not supposed to do any heavy work. 💼", "El Sr. Bruno está mucho mejor después de su operación, pero todavía no se supone que haga ningún trabajo pesado. 💼"],
+  ];
+  
+
+export itIsSaidThat;
