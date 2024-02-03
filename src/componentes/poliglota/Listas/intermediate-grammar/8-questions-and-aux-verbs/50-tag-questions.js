fab34232a5e6b6b7c66139a91ca019b73569b90b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-02 19:04:27 -0600

    Content: add 50 tag questions

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/8-questions-and-aux-verbs/50-tag-questions.js
@@ -0,0 +1,28 @@
+// 50 Tag Questions (do you? / isn’t it? etc.)
+
+const tagQuestions = [
+    ['You haven’t seen Maria today, have you? –No, I haven’t. 👀❓', 'No has visto a María hoy, ¿verdad? –No, no la he visto. 👀❓'],
+    ['It was a good movie, wasn’t it? –Yes, I loved it. 🎬👍', 'Fue una buena película, ¿verdad? –Sí, me encantó. 🎬👍'],
+    ['Lauren plays the piano, doesn’t she? –Well, yes, but not very well. 🎹🤔', 'Lauren toca el piano, ¿verdad? –Bueno, sí, pero no muy bien. 🎹🤔'],
+    ['You didn’t lock the door, did you? - No, I forgot. 🔒😳', 'No cerraste la puerta, ¿verdad? –No, me olvidé. 🔒😳'],
+    ['Maria will be here soon, won’t she? 🕒🤔', 'María estará aquí pronto, ¿verdad? 🕒🤔'],
+    ['There was a lot of traffic, wasn’t there? 🚗🤔', 'Hubo mucho tráfico, ¿verdad? 🚗🤔'],
+    ['Jim should take his medicine, shouldn’t he? 💊🤔', 'Jim debería tomar su medicina, ¿no? 💊🤔'],
+    ['Kate won’t be late, will she? ⏰🤔', 'Kate no llegará tarde, ¿verdad? ⏰🤔'],
+    ['They don’t like us, do they? 👎🤔', 'Ellos no nos quieren, ¿verdad? 👎🤔'],
+    ['You haven’t paid the gas bill, have you? 💸❓', 'No has pagado la factura del gas, ¿verdad? 💸❓'],
+    ['You’re not going out today, are you? –Yes. 🚶‍♂️❓', 'No vas a salir hoy, ¿verdad? –Sí. 🚶‍♂️❓'],
+    ['You’re not going to play videogames today, are you? –No. 🎮❓', 'No vas a jugar videojuegos hoy, ¿verdad? –No. 🎮❓'],
+    ['It’s a nice day, isn’t it? –Yes, beautiful. 🌞👌', 'Es un buen día, ¿verdad? –Sí, hermoso. 🌞👌'],
+    ['Eric doesn’t look too good today, does he? –No, he looks very tired. 😴🤔', 'Eric no se ve muy bien hoy, ¿verdad? –No, se ve muy cansado. 😴🤔'],
+    ['She’s very funny. She has a wonderful sense of humor, doesn’t she? 🤣👍', 'Ella es muy divertida. Tiene un sentido del humor maravilloso, ¿verdad? 🤣👍'],
+    ['You haven’t seen Lisa today, have You? –No, I haven’t. 👀❓', 'No has visto a Lisa hoy, ¿verdad? –No, no la he visto. 👀❓'],
+    ['You wouldn’t have a pen, would you? –Yes, here you are. ✒️👀', 'No tendrías un bolígrafo, ¿verdad? –Sí, aquí tienes. ✒️👀'],
+    ['You couldn’t lend me some money, could you? –It depends how much. 💰🤔', 'No podrías prestarme algo de dinero, ¿verdad? –Depende de cuánto. 💰🤔'],
+    ['You don’t know where Lauren is, do you? –Sorry, I have no Idea. 🤷‍♂️🤷‍♀️', 'No sabes dónde está Lauren, ¿verdad? –Lo siento, no tengo ni idea. 🤷‍♂️🤷‍♀️'],
+    ['Let’s go for a walk. shall we? 🚶‍♂️🚶‍♀️', 'Vamos a dar un paseo. ¿Te parece? 🚶‍♂️🚶‍♀️'],
+    ['Listen to me, will you? 👂🙏', 'Escúchame, ¿quieres? 👂🙏'],
+    ['I’m right, aren’t I?. –Yes, you are. ✅👍', 'Tengo razón, ¿no es así? –Sí, tienes razón. ✅👍'],
+  ];
+  
+export default tagQuestions;
