0071dd33b3574d8ceeb11d79a080b27d18256e38
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-05 22:00:28 -0600

    Content: add 57 Prefer and would rather.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/8-questions-and-aux-verbs/57-prefer-and-would-rather.js
@@ -0,0 +1,26 @@
+// 57 Prefer and would rather
+
+const preferAndWouldRather = [
+    ['I don’t like cities. I prefer to live in the country. 😞🏙️', 'No me gustan las ciudades. Prefiero vivir en el campo. 😞🏙️'],
+    ['I prefer this coat to the coat you were wearing yesterday. 👕🤔', 'Prefiero este abrigo al abrigo que llevabas ayer. 👕🤔'],
+    ['I prefer driving to traveling by train. 🚗🚆', 'Prefiero conducir a viajar en tren. 🚗🚆'],
+    ['I prefer to drive rather than travel by train. 🚗🤔', 'Prefiero conducir en lugar de viajar en tren. 🚗🤔'],
+    ['Ann prefers to live in the country rather than in a city. 👩‍🌾🌳', 'Ann prefiere vivir en el campo en lugar de en la ciudad. 👩‍🌾🌳'],
+    ['Would you prefer tea or coffee? –Coffee, please. ☕🤔', '¿Prefieres té o café? –Café, por favor. ☕🤔'],
+    ['Should we take the train? –No, I’d prefer to drive. 🚂🚗', '¿Deberíamos tomar el tren? –No, prefiero conducir. 🚂🚗'],
+    ['I’d prefer to stay at home tonight rather than go to the movies. 🏠🎥', 'Prefiero quedarme en casa esta noche en lugar de ir al cine. 🏠🎥'],
+    ['Should we take the train? –I’d rather drive. 🚂🚗', '¿Deberíamos tomar el tren? –Prefiero conducir. 🚂🚗'],
+    ['Would you rather have tea or coffee? –Coffee, please. ☕🤔', '¿Preferirías té o café? –Café, por favor. ☕🤔'],
+    ['I’m tired. I’d rather not go out tonight, if you don’t mind. 😴🚫', 'Estoy cansado. Preferiría no salir esta noche, si no te importa. 😴🚫'],
+    ['Do you want to go out tonight? –I’d rather not. 🌃🤔', '¿Quieres salir esta noche? –Prefiero no hacerlo. 🌃🤔'],
+    ['I’d rather stay at home tonight than go to the movies. 🏠🎥', 'Prefiero quedarme en casa esta noche en lugar de ir al cine. 🏠🎥'],
+    ['I’ll fix your car tomorrow, OK? –I’d rather you did it today. 🚗🔧', '¿Arreglo tu auto mañana, vale? –Preferiría que lo hicieras hoy. 🚗🔧'],
+    ['Is it OK if Ben stays here? –I’d rather he came with us. 🤷‍♂️🏠', '¿Está bien si Ben se queda aquí? –Preferiría que viniera con nosotros. 🤷‍♂️🏠'],
+    ['Shall I tell them, or would you rather they didn’t know? 🤔🗣️', '¿Debo decírselo, o preferirías que no lo supieran? 🤔🗣️'],
+    ['I’d rather make dinner now. 🍲🤔', 'Prefiero hacer la cena ahora. 🍲🤔'],
+    ['I’d rather you made dinner now. 🍲🤔', 'Prefiero que hagas la cena ahora. 🍲🤔'],
+    ['I’d rather you didn’t tell anyone what I said. 🤫🚫', 'Preferiría que no le dijeras a nadie lo que dije. 🤫🚫'],
+    ['Should I tell Stephanie? –I’d rather you didn’t. 🤔🤫', '¿Debería decírselo a Stephanie? –Preferiría que no lo hicieras. 🤔🤫'],
+  ];
+  
+export default preferAndWouldRather;
