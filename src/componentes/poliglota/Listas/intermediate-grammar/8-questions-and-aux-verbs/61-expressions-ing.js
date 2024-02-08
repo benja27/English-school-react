353ae3b440bec7564fec84413aa165dd31d0e693
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-07 21:57:15 -0600

    Content: add 61 Expressions + ing ..

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/8-questions-and-aux-verbs/61-expressions-ing.js
@@ -0,0 +1,26 @@
+// 61 Expressions + -ing
+
+const expressionsIng = [
+    ['There’s no point in having a car if you never use it. 🚗❌', 'No tiene sentido tener un coche si nunca lo usas. 🚗❌'],
+    ['There was no point in waiting any longer, so we left. 🕒❌', 'No tenía sentido esperar más, así que nos fuimos. 🕒❌'],
+    ['What’s the point of having a car if you never use it? 🤔❓', '¿Cuál es el punto de tener un coche si nunca lo usas? 🤔❓'],
+    ['There’s nothing you can do about the situation, so there’s no use worrying about it. 🤷‍♂️❌', 'No puedes hacer nada sobre la situación, así que no sirve de nada preocuparse por ella. 🤷‍♂️❌'],
+    ['I live only a short walk from here, so it’s not worth taking a taxi. 🚶‍♂️🚖', 'Vivo a solo un corto paseo de aquí, así que no vale la pena tomar un taxi. 🚶‍♂️🚖'],
+    ['Our flight was very early in the morning, so it wasn’t worth going to bed. ✈️😴', 'Nuestro vuelo era muy temprano en la mañana, así que no valía la pena ir a la cama. ✈️😴'],
+    ['What was the movie like? Was it worth seeing? 🎬🤔', '¿Cómo estuvo la película? ¿Valió la pena verla? 🎬🤔'],
+    ['Thieves broke into the house but didn’t take anything. There was nothing worth stealing. 👮‍♂️🏠', 'Los ladrones entraron a la casa pero no se llevaron nada. No había nada que valiera la pena robar. 👮‍♂️🏠'],
+    ['I had no trouble finding a place to live. 🏠😊', 'No tuve ningún problema para encontrar un lugar donde vivir. 🏠😊'],
+    ['Did you have any trouble getting a visa? 🛂🤔', '¿Tuviste algún problema para obtener una visa? 🛂🤔'],
+    ['People often have a lot of trouble reading my writing. 👀📝', 'A menudo las personas tienen muchos problemas para leer mi escritura. 👀📝'],
+    ['I had difficulty finding a place to live. 🏠😟', 'Tuve dificultades para encontrar un lugar donde vivir. 🏠😟'],
+    ['I had a problem finding a place to live. 🏠😞', 'Tuve un problema para encontrar un lugar donde vivir. 🏠😞'],
+    ['He spent hours trying to repair the clock. ⏰⏳', 'Pasó horas tratando de reparar el reloj. ⏰⏳'],
+    ['I waste a lot of time daydreaming. 💭😔', 'Pierdo mucho tiempo soñando despierto. 💭😔'],
+    ['She said she couldn’t go with us. She was too busy doing other things. 🙅‍♀️🕒', 'Dijo que no podía ir con nosotros. Estaba demasiado ocupada haciendo otras cosas. 🙅‍♀️🕒'],
+    ['How often do you go swimming? 🏊‍♂️🤔', '¿Con qué frecuencia nadas? 🏊‍♂️🤔'],
+    ['I’d like to go skiing. ⛷️🙂', 'Me gustaría ir a esquiar. ⛷️🙂'],
+    ['When was the last time you went shopping? 🛍️🤔', '¿Cuándo fue la última vez que fuiste de compras? 🛍️🤔'],
+    ['I’ve never gone sailing. ⛵🤷‍♂️', 'Nunca he ido a navegar. ⛵🤷‍♂️'],
+  ];  
+
+export default expressionsIng;
