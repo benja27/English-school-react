d340988dd50b80d25f49c21af46dcf185014657d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-08 20:45:35 -0600

    Content: Add 65 See somebody do and see somebody doing

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/8-questions-and-aux-verbs/65-see-somebody-do-doing.js
@@ -0,0 +1,21 @@
+// 65 See somebody do and see somebody doing
+
+const seeSomebodyDoDoing = [
+    ['I saw Tom get into his car and drive away. 👀🚗', 'Vi a Tom subirse a su coche y conducir lejos. 👀🚗'],
+    ['I saw somebody do something. 👀👤', 'Vi a alguien hacer algo. 👀👤'],
+    ['I saw Kate waiting for a bus. 👀🚌', 'Vi a Kate esperando un autobús. 👀🚌'],
+    ['He fell off the wall. I saw him fall off the wall. 🧱😲', 'Se cayó del muro. Lo vi caerse del muro. 🧱😲'],
+    ['Did you see the accident happen? 👀🚗', '¿Viste el accidente suceder? 👀🚗'],
+    ['He was walking along the street. I saw him walking along the street. 🚶‍♂️🏙️', 'Él caminaba por la calle. Lo vi caminar por la calle. 🚶‍♂️🏙️'],
+    ['I’ve never seen her dance. / I’ve never seen her dancing. 👀💃', 'Nunca la he visto bailar. / Nunca la he visto bailando. 👀💃'],
+    ['I didn’t hear you come in. 👂🚪', 'No te oí entrar. 👂🚪'],
+    ['Liz suddenly felt somebody touch her on the shoulder. 😮👐', 'Liz de repente sintió a alguien tocarle en el hombro. 😮👐'],
+    ['Did you notice anyone go out? 👀🚶‍♂️', '¿Notaste a alguien salir? 👀🚶‍♂️'],
+    ['I could hear it raining. 👂🌧️', 'Podía oír que llovía. 👂🌧️'],
+    ['The missing children were last seen playing near the river. 👀👧👦', 'Se vio por última vez a los niños desaparecidos jugando cerca del río. 👀👧👦'],
+    ['Listen to the birds singing! 🐦🎶', '¡Escucha a los pájaros cantando! 🐦🎶'],
+    ['Can you smell something burning? 👃🔥', '¿Puedes oler algo quemándose? 👃🔥'],
+    ['I found Sue in my room reading my letters. 👀📃', 'Encontré a Sue en mi habitación leyendo mis cartas. 👀📃'],
+  ];  
+
+export default seeSomebodyDoDoing;
