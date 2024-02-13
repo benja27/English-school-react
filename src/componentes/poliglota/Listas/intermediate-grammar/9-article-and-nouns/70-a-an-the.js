9924b7c4cab96c767a6f46553bb6789740cdc639
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-12 21:32:13 -0600

    Content: add 70 A/an and the

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/70-a-an-the.js
@@ -0,0 +1,28 @@
+// A/an and the
+
+const aAnThe = [
+    ['I had a sandwich and an apple for lunch. The sandwich wasn’t very good, but the apple was delicious. 🥪🍏', 'Almorcé un sándwich y una manzana. El sándwich no estaba muy bueno, pero la manzana estaba deliciosa. 🥪🍏'],
+    ['A man and a woman were sitting across from me. The man was American, but I think the woman was British. 👨👩', 'Un hombre y una mujer estaban sentados frente a mí. El hombre era estadounidense, pero creo que la mujer era británica. 👨👩'],
+    ['When we were on vacation, we stayed at a hotel. Sometimes we ate at the hotel, and sometimes we went to a restaurant. 🏨🍽️', 'Cuando estábamos de vacaciones, nos quedamos en un hotel. A veces comíamos en el hotel, y a veces íbamos a un restaurante. 🏨🍽️'],
+    ['Tim sat down on a chair. 🪑', 'Tim se sentó en una silla. 🪑'],
+    ['Tim sat down on the chair nearest the door. 🚪🪑', 'Tim se sentó en la silla más cercana a la puerta. 🚪🪑'],
+    ['Paula is looking for a job. 👀💼', 'Paula está buscando trabajo. 👀💼'],
+    ['Did Paula get the job she applied for? 🤔💼', '¿Paula consiguió el trabajo para el que aplicó? 🤔💼'],
+    ['Do you have a car? 🚗', '¿Tienes un coche? 🚗'],
+    ['I washed the car yesterday. 🧼🚗', 'Lavé el coche ayer. 🧼🚗'],
+    ['Can you turn off the light, please? 💡', '¿Puedes apagar la luz, por favor? 💡'],
+    ['I took a taxi to the station. 🚖🚉', 'Tomé un taxi a la estación. 🚖🚉'],
+    ['I’d like to speak to the manager, please. 👩‍💼🗣️', 'Me gustaría hablar con el gerente, por favor. 👩‍💼🗣️'],
+    ['I have to go to the bank and then I’m going to the post office. 🏦📬', 'Tengo que ir al banco y luego voy a la oficina de correos. 🏦📬'],
+    ['Carol isn’t very well. She went to the doctor. 🤒👩‍⚕️', 'Carol no está muy bien. Fue al médico. 🤒👩‍⚕️'],
+    ['Two people were taken to the hospital after the accident. 🏥🚑', 'Dos personas fueron llevadas al hospital después del accidente. 🏥🚑'],
+    ['I have to go to the bank today. 🏦', 'Tengo que ir al banco hoy. 🏦'],
+    ['Is there a bank near here? 🏦📍', '¿Hay un banco cerca de aquí? 🏦📍'],
+    ['I don’t like going to the dentist. 😬🦷', 'No me gusta ir al dentista. 😬🦷'],
+    ['My sister is a dentist. 👩‍⚕️🦷', 'Mi hermana es dentista. 👩‍⚕️🦷'],
+    ['How often do you go to the movies? –about once a month. 🎬📅', '¿Con qué frecuencia vas al cine? - Aproximadamente una vez al mes. 🎬📅'],
+    ['How much are those potatoes? –A dollar a pound. 🥔💲', '¿Cuánto cuestan esas papas? - Un dólar la libra. 🥔💲'],
+    ['Helen works eight hours a day, six days a week. 👩‍💼⏰', 'Helen trabaja ocho horas al día, seis días a la semana. 👩‍💼⏰'],
+  ];
+  
+export default aAnThe;
