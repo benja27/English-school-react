03297a71619ae591d32f6a654fa365f2d760be79
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-12 21:34:19 -0600

    Content: add 71 the 1

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/71-the.js
@@ -0,0 +1,32 @@
+// 71 The 1
+
+const the = [
+    ['What is the longest river in the world? 🌊🌍', '¿Cuál es el río más largo del mundo? 🌊🌍'],
+    ['The Earth goes around the sun, and the moon goes around the Earth. 🌎🌞🌛', 'La Tierra gira alrededor del sol y la luna gira alrededor de la Tierra. 🌎🌞🌛'],
+    ['Have you ever crossed the equator? 🌐', '¿Alguna vez has cruzado el ecuador? 🌐'],
+    ['I’m going away at the end of this month. ✈️📅', 'Me voy a ir a fin de mes. ✈️📅'],
+    ['Paris is the capital of France. 🇫🇷🗼', 'París es la capital de Francia. 🇫🇷🗼'],
+    ['The sun is a star. ☀️⭐', 'El sol es una estrella. ☀️⭐'],
+    ['The hotel we stayed at was a very nice hotel. 🏨😊', 'El hotel en el que nos alojamos era un hotel muy bonito. 🏨😊'],
+    ['We looked up at all the stairs in the sky. 🌌👀', 'Miramos todas las estrellas en el cielo. 🌌👀'],
+    ['Would you like to live in the country? 🏞️🏠', '¿Te gustaría vivir en el campo? 🏞️🏠'],
+    ['We must do more to protect the environment. 🌳🌱', 'Debemos hacer más para proteger el medio ambiente. 🌳🌱'],
+    ['There are millions of stars in space. ✨🌌', 'Hay millones de estrellas en el espacio. ✨🌌'],
+    ['I tried to park my cay, but the space was too small. 🚗🅿️', 'Intenté estacionar mi auto, pero el espacio era demasiado pequeño. 🚗🅿️'],
+    ['Your sweater is the same color as mine. 👚🎨', 'Tu suéter es del mismo color que el mío. 👚🎨'],
+    ['Are these keys the same? No, they’re different. 🔑❌', '¿Estas llaves son iguales? No, son diferentes. 🔑❌'],
+    ['I go to the movies a lot, but I haven’t been to the theater in ages. 🎬🎭', 'Voy mucho al cine, pero no he ido al teatro en mucho tiempo. 🎬🎭'],
+    ['I listen to the radio a lot. 📻🎶', 'Escucho la radio mucho. 📻🎶'],
+    ['I watch television a lot. 📺👀', 'Miro la televisión mucho. 📺👀'],
+    ['We heard the news on the radio. 📰📻', 'Escuchamos las noticias en la radio. 📰📻'],
+    ['We watched the news on TV. 📰📺', 'Vimos las noticias en la televisión. 📰📺'],
+    ['Can you turn off the television, please? 📺❌', '¿Puedes apagar la televisión, por favor? 📺❌'],
+    ['What did you have for breakfast? 🍳🥞', '¿Qué desayunaste? 🍳🥞'],
+    ['We had lunch in a very nice restaurant. 🍽️😋', 'Almorzamos en un restaurante muy agradable. 🍽️😋'],
+    ['What time is dinner? 🍽️🕔', '¿A qué hora es la cena? 🍽️🕔'],
+    ['We had a very nice lunch. 🍽️😊', 'Tuvimos un almuerzo muy agradable. 🍽️😊'],
+    ['Our plane leaves from Gate 10. ✈️🛫', 'Nuestro avión sale de la Puerta 10. ✈️🛫'],
+    ['Do you have these shoes in size 9? 👞👟', '¿Tienes estos zapatos en talla 9? 👞👟'],
+  ];
+  
+export default the;
