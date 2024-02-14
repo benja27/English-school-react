b59ad5bf4d525d86aed304ce4639fa9719161f1e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-13 21:12:21 -0600

    Content: add 74 the 4 the giraffe / the telephone / the piano /, etc., the + Adjective)

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/74-the-4.js
@@ -0,0 +1,19 @@
+// 74 the 4 the giraffe / the telephone / the piano, etc., the + Adjective)
+
+const the = [
+    ['The giraffe is the tallest of all animals. 🦒📏', 'La jirafa es el animal más alto de todos. 🦒📏'],
+    ['The bicycle is an excellent means of transportation. 🚲🛣️', 'La bicicleta es un excelente medio de transporte. 🚲🛣️'],
+    ['When was the telephone invented? 📞🤔', '¿Cuándo se inventó el teléfono? 📞🤔'],
+    ['The dollar is the currency of the United States. 💵🇺🇸', 'El dólar es la moneda de los Estados Unidos. 💵🇺🇸'],
+    ['Can you play the guitar? 🎸🎶', '¿Puedes tocar la guitarra? 🎸🎶'],
+    ['The piano is my favorite instrument. 🎹❤️', 'El piano es mi instrumento favorito. 🎹❤️'],
+    ['I’d like to have a piano but I can’t play the piano. 🎹😔', 'Me gustaría tener un piano, pero no sé tocar el piano. 🎹😔'],
+    ['We saw a giraffe at the zoo and the giraffe is my favorite animal. 🦒👀', 'Vimos una jirafa en el zoológico y la jirafa es mi animal favorito. 🦒👀'],
+    ['What do you know about the origins of man? 🤔🌍', '¿Qué sabes sobre los orígenes del hombre? 🤔🌍'],
+    ['Do you think the rich should pay higher taxes? 💰💸', '¿Crees que los ricos deberían pagar impuestos más altos? 💰💸'],
+    ['The government has promised to provide more money to help the homeless. 🏠💵', 'El gobierno ha prometido proporcionar más dinero para ayudar a las personas sin hogar. 🏠💵'],
+    ['The French are famous for their food. 🇫🇷🍽️', 'Los franceses son famosos por su comida. 🇫🇷🍽️'],
+    ['The Chinese invented printing. 🇨🇳📜', 'Los chinos inventaron la imprenta. 🇨🇳📜'],
+  ];
+  
+export default the;
