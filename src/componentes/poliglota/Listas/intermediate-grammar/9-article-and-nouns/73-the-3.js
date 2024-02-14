f69ac7805186534bbfe32608b90fc4cbf22a2370
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-13 21:10:09 -0600

    Content: Add 73 the 3 (children / the children)

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/73-the-3.js
@@ -0,0 +1,24 @@
+// 73 the 3 (children / the children)
+
+const the = [
+    ['I’m afraid of dogs. 🐕😨', 'Tengo miedo de los perros. 🐕😨'],
+    ['Doctors are paid more than teachers. 👩‍⚕️💰👨‍🏫', 'Los médicos ganan más que los maestros. 👩‍⚕️💰👨‍🏫'],
+    ['Do you collect stamps? 📬💌', '¿Coleccionas estampillas? 📬💌'],
+    ['Crime is a problem in most big cities. 👮‍♂️🏙️', 'El crimen es un problema en la mayoría de las grandes ciudades. 👮‍♂️🏙️'],
+    ['Life has changed a lot in the last 30 years. 🔄📅', 'La vida ha cambiado mucho en los últimos 30 años. 🔄📅'],
+    ['Do you like classical music ? 🎼❤️', '¿Te gusta la música clásica? 🎼❤️'],
+    ['Do you like Chinese food? 🥡🍜', '¿Te gusta la comida china? 🥡🍜'],
+    ['Do you like fast cars? 🏎️😎', '¿Te gustan los autos rápidos? 🏎️😎'],
+    ['My favorite sport is football. ⚽🥅', 'Mi deporte favorito es el fútbol. ⚽🥅'],
+    ['My favorite subject at school was history. 🏫📚', 'Mi materia favorita en la escuela era historia. 🏫📚'],
+    ['Most hotels accept credit cards. 💳🏨', 'La mayoría de los hoteles aceptan tarjetas de crédito. 💳🏨'],
+    ['We took the children to the zoo. 🐒🦁', 'Llevamos a los niños al zoológico. 🐒🦁'],
+    ['The movie wasn’t very good, but I liked the music. 🎬🎶', 'La película no fue muy buena, pero me gustó la música. 🎬🎶'],
+    ['All the cars in this parking lot belong to people who work here. 🅿️🚗', 'Todos los autos en este estacionamiento pertenecen a personas que trabajan aquí. 🅿️🚗'],
+    ['Can you pass the sugar, please? 🍚🥄', '¿Puedes pasar el azúcar, por favor? 🍚🥄'],
+    ['Do the Americans you know drink tea? 🇺🇸🍵', '¿Los estadounidenses que conoces toman té? 🇺🇸🍵'],
+    ['I like the people I work with. 👩‍💼👨‍💼', 'Me gusta la gente con la que trabajo. 👩‍💼👨‍💼'],
+    ['Did you like the coffee we had after dinner last night? ☕🌙', '¿Te gustó el café que tomamos después de la cena anoche? ☕🌙'],
+  ];
+  
+export default the;
