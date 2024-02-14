b547717291fbc610d744fc9a4104e18a6463dda6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-13 21:14:32 -0600

    Content: add 75 Names with and without the 1

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/75-names-with-and-without-the.js
@@ -0,0 +1,17 @@
+// 75 Names with and without the 1
+
+const namesWithAndWithoutThe =  [
+    ['The Czech Republic. 🇨🇿', 'La República Checa. 🇨🇿'],
+    ['The United Kingdom. 🇬🇧', 'El Reino Unido. 🇬🇧'],
+    ['The Dominican Republic. 🇩🇴', 'La República Dominicana. 🇩🇴'],
+    ['The United States of America. 🇺🇸', 'Los Estados Unidos de América. 🇺🇸'],
+    ['We visited Canada and the United States. 🇨🇦🇺🇸', 'Visitamos Canadá y los Estados Unidos. 🇨🇦🇺🇸'],
+    ['We called the doctor. 📞👩‍⚕️', 'Llamamos al médico. 📞👩‍⚕️'],
+    ['We called Doctor Johnson. 📞👨‍⚕️', 'Llamamos al Doctor Johnson. 📞👨‍⚕️'],
+    ['They live near the lake. 🏞️🏡', 'Ellos viven cerca del lago. 🏞️🏡'],
+    ['They live near Lake Superior. 🏞️🏡', 'Ellos viven cerca del lago Superior. 🏞️🏡'],
+    ['The highest mountain in the Andes is Mount Aconcagua. 🏔️', 'La montaña más alta de los Andes es el Monte Aconcagua. 🏔️'],
+    ['Sweden is in northern Europe; Spain is in the south. 🇸🇪🇪🇸', 'Suecia está en el norte de Europa; España está en el sur. 🇸🇪🇪🇸'],
+  ];
+  
+export default namesWithAndWithoutThe;
