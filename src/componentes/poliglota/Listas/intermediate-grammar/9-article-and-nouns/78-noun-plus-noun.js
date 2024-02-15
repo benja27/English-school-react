22fd410ee52e616ecec43ec50e437e11ca54c758
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-14 22:07:05 -0600

    Content: add 78 Noun + noun (a tennis ball/ a headache, etc.)

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/78-noun-plus-noun.js
@@ -0,0 +1,11 @@
+// 78 Noun + Noun (a tennis ball / a headache, etc.)
+
+const nounPlusNoun = [
+    ['I waited at the hotel reception desk. 🏨⏳', 'Esperé en la recepción del hotel. 🏨⏳'],
+    ['We watches the World Swimming Championships on television. 🏊‍♂️📺', 'Vimos los Campeonatos Mundiales de Natación en televisión. 🏊‍♂️📺'],
+    ['Everyone is talking about the government corruption scandal. 👥💬', 'Todos están hablando del escándalo de corrupción gubernamental. 👥💬'],
+    ['A ticket for a concert is a concert ticket. 🎫🎵', 'Un boleto para un concierto es un boleto de concierto. 🎫🎵'],
+    ['Problems concerning health are health problems. 💊🤕', 'Los problemas relacionados con la salud son problemas de salud. 💊🤕'],
+  ];  
+
+export default nounPlusNoun;
