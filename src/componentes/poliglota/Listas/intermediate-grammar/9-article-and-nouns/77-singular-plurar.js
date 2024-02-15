e01b69d0a230933a92c1afbcc6dc5da50fa816c7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-14 22:04:55 -0600

    Content: Add 77 Singular and Plural

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/9-article-and-nouns/77-singular-plurar.js
@@ -0,0 +1,20 @@
+// Singular and plural
+
+const singularPlural = [
+    ['My pants are too long. 👖😕', 'Mis pantalones son demasiado largos. 👖😕'],
+    ['Those are nice jeans. 👖👍', 'Esos son unos vaqueros bonitos. 👖👍'],
+    ['That’s a nice pair of jeans. 👖👌', 'Ese es un buen par de vaqueros. 👖👌'],
+    ['I need some new glasses. 👓😞', 'Necesito unas gafas nuevas. 👓😞'],
+    ['I need a new pair of glasses. 👓😔', 'Necesito un par de gafas nuevo. 👓😔'],
+    ['Gymnastics is my favorite sport. 🤸‍♂️❤️', 'La gimnasia es mi deporte favorito. 🤸‍♂️❤️'],
+    ['What time is the news on television? 📺⏰', '¿A qué hora son las noticias en televisión? 📺⏰'],
+    ['The police are investigating the murder, but haven’t arrested anyone yet. 👮‍♂️🔍', 'La policía está investigando el asesinato, pero aún no ha arrestado a nadie. 👮‍♂️🔍'],
+    ['He’s a nice person. 👨🙂', 'Es una persona agradable. 👨🙂'],
+    ['They are nice people. 👫🙂', 'Son personas agradables. 👫🙂'],
+    ['Many people don’t have enough to eat. 🍽️😔', 'Muchas personas no tienen suficiente para comer. 🍽️😔'],
+    ['Twenty thousand dollars was stolen in the robbery. 💰😱', 'Veinte mil dólares fueron robados en el atraco. 💰😱'],
+    ['Three years is a long time to be without a job. ⌛😞', 'Tres años es mucho tiempo para estar sin trabajo. ⌛😞'],
+    ['Six miles is a long way to walk every day. 🚶‍♂️👣', 'Seis millas es un largo camino para caminar todos los días. 🚶‍♂️👣'],
+  ];
+  
+export default singularPlural;
