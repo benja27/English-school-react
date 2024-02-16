c7554a0edf62d0c2fc75721d0a3df8ddf8cd6053
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-15 20:35:45 -0600

    Content: Add 82 There ... and It ...

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/10-pronouns-determiners/82-there-it.js
@@ -0,0 +1,32 @@
+// 82 There … and It …
+
+const thereIt = [
+    ["There’s a new restaurant on Main Street. 🍽️", "Hay un restaurante nuevo en la calle principal."],
+    ["I’m sorry I’m late. There was a lot of traffic. ⏰", "Lo siento por llegar tarde. Había mucho tráfico."],
+    ["Things are more expensive now. There has been a big increase in the cost of living. 💸", "Las cosas son más caras ahora. Ha habido un gran aumento en el costo de vida."],
+    ["We went to the new restaurant. It’s very good. 🌟", "Fuimos al nuevo restaurante. Es muy bueno."],
+    ["I wasn’t expecting them to come. It was a complete surprise. 🎉", "No esperaba que vinieran. Fue una completa sorpresa."],
+    ["I don’t like this town. There’s nothing to do here. It’s boring place. 😒", "No me gusta esta ciudad. No hay nada que hacer aquí. Es un lugar aburrido."],
+    ["The new restaurant is very good. I went there last night. 🌃", "El nuevo restaurante es muy bueno. Fui allí anoche."],
+    ["When we got to the party, there were already a lot of people there. 🎊", "Cuando llegamos a la fiesta, ya había mucha gente allí."],
+    ["Will there be many people at the party? 🎈", "¿Habrá mucha gente en la fiesta?"],
+    ["Is there a flight to Miami tonight? –There might be. I’ll check. ✈️", "¿Hay un vuelo a Miami esta noche? -Podría ser. Voy a comprobarlo."],
+    ["If people drove more carefully, there wouldn’t be so many accidents. 🚗", "Si la gente condujera con más cuidado, no habría tantos accidentes."],
+    ["There was music playing. There must have been somebody at home. 🎵", "Había música sonando. Debe haber alguien en casa."],
+    ["They live on a busy street. There must be a lot of noise from the traffic. 🚦", "Viven en una calle muy transitada. Debe haber mucho ruido del tráfico."],
+    ["They live on a busy main street. It must be very noisy. 🛣️", "Viven en una calle principal muy transitada. Debe ser muy ruidoso."],
+    ["There used to be a movie theater on Main Street, but it closed a few years ago. 🎥", "Solía haber un cine en la calle principal, pero cerró hace unos años."],
+    ["That building is now a supermarket. It used to be a movie theater. 🏢", "Ese edificio ahora es un supermercado. Solía ser un cine."],
+    ["There is bound to be a flight to Miami tonight. ✈️", "Debe haber un vuelo a Miami esta noche."],
+    ["It’s dangerous to walk in the street. ⚠️", "Es peligroso caminar en la calle."],
+    ["It didn’t take us long to get here. 🕒", "No nos llevó mucho tiempo llegar aquí."],
+    ["It’s too bad Sandra can’t come to the party. 😞", "Es una lástima que Sandra no pueda venir a la fiesta."],
+    ["Let’s go. It’s not worth waiting any longer. 🚶‍♂️", "Vámonos. No vale la pena esperar más."],
+    ["How far is it from here to the airport? ✈️", "¿Qué tan lejos está desde aquí hasta el aeropuerto?"],
+    ["What day is it today? 📅", "¿Qué día es hoy?"],
+    ["It’s been a long time since I saw you. ⏳", "Ha pasado mucho tiempo desde que te vi."],
+    ["It was windy. 🌬️", "Hacía viento."],
+    ["There was a cold wind. 🌬️", "Había un viento frío."],
+  ];
+  
+export default thereIt;
