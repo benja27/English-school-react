79a289d85aa8d4495685d608ccf78bfe9a0b51c8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-15 20:30:28 -0600

    Content: add 80 Myself / Yourself/ Themselves, etc.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/10-pronouns-determiners/80-myself-yourself-themselves.js
@@ -0,0 +1,26 @@
+// 80 Myself/yourself/themselves, etc.
+
+const myselfYourselfThemselves = [
+    ["I don't want you to pay for me. I'll pay for myself. 💰", "No quiero que pagues por mí. Yo pagaré por mí mismo. 💰"],
+    ["Julia had a great vacation. She really enjoyed herself. 🏖️", "Julia tuvo unas vacaciones geniales. Realmente disfrutó. 🏖️"],
+    ["Do you talk to yourself sometimes? 🗣️", "¿Hablas contigo mismo(a) a veces? 🗣️"],
+    ["If you want more to eat, help yourselves. 🍽️", "Si quieren más para comer, sírvanse ustedes mismos. 🍽️"],
+    ["It's not our fault. You can't blame us. 🤷‍♂️", "No es nuestra culpa. No puedes culparnos. 🤷‍♂️"],
+    ["It's our own fault. We should blame ourselves. 🙇‍♂️", "Es nuestra propia culpa. Deberíamos culparnos a nosotros mismos. 🙇‍♂️"],
+    ["I feel nervous. I can't relax. 😟", "Me siento nervioso(a). No puedo relajarme. 😟"],
+    ["You have to try and concentrate. 💡", "Debes intentar concentrarte. 💡"],
+    ["What time should we meet? ⌚", "¿A qué hora deberíamos encontrarnos? ⌚"],
+    ["He got up, washed, shaved, and dressed. 🚿", "Se levantó, se lavó, se afeitó y se vistió. 🚿"],
+    ["Kate and Joe stood in front of the mirror and looked at themselves. 👫", "Kate y Joe se pararon frente al espejo y se miraron a sí mismos. 👫"],
+    ["Kate looked at Joe; Joe looked at Kate. They looked at each other. 👀", "Kate miró a Joe; Joe miró a Kate. Se miraron el uno al otro. 👀"],
+    ["How long have you and Bill known each other? 🕰️", "¿Hace cuánto tiempo se conocen tú y Bill? 🕰️"],
+    ["Sue and Ann don't like each other. 😡", "Sue y Ann no se caen bien. 😡"],
+    ["Do you and Sarah live near each other? 🏠", "¿Tú y Sarah viven cerca uno del otro? 🏠"],
+    ["Who repaired your bicycle for you? –I repaired it myself. 🚲", "¿Quién reparó tu bicicleta por ti? –Yo mismo la reparé. 🚲"],
+    ["I'm not going to do your work for you. You can do it yourself. 💼", "No voy a hacer tu trabajo por ti. Puedes hacerlo tú mismo. 💼"],
+    ["Let's paint the house ourselves. It will be much cheaper. 🏡", "Pintemos la casa nosotros mismos. Será mucho más barato. 🏡"],
+    ["The movie itself wasn't very good, but I loved the music. 🎬", "La película en sí misma no fue muy buena, pero me encantó la música. 🎬"],
+    ["I don't think Sue will get the job. Sue herself doesn't think she'll get it. 💼", "No creo que Sue consiga el trabajo. Sue misma no cree que lo vaya a conseguir. 💼"],
+  ];  
+
+export default myselfYourselfThemselves;
