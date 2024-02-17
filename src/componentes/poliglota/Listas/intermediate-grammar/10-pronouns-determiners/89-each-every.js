d7b5abb197104e88002aba3f1cabe1a891312bf1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-16 18:59:07 -0600

    Content: add 89 Each and every

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/10-pronouns-determiners/89-each-every.js
@@ -0,0 +1,19 @@
+// 89 Each and every
+
+const eachEvery = [
+    ['Each time I see you, you look different. 😀👀', 'Cada vez que te veo, pareces diferente. 😀👀'],
+    ['There’s a ceiling fan in each room of the house. 🏠🌀', 'Hay un ventilador de techo en cada habitación de la casa. 🏠🌀'],
+    ['In a baseball game, each team has nine players. ⚾👥', 'En un juego de béisbol, cada equipo tiene nueve jugadores. ⚾👥'],
+    ['How often do you use your computer? –every day. 💻🔄', '¿Con qué frecuencia usas tu computadora? –todos los días. 💻🔄'],
+    ['There’s a bus every 10 minutes. 🚌🕒', 'Hay un autobús cada 10 minutos. 🚌🕒'],
+    ['Read each of these sentences carefully. 📖🔍', 'Lee cada una de estas oraciones cuidadosamente. 📖🔍'],
+    ['I’ve read every one of those books. 📚👀', 'He leído cada uno de esos libros. 📚👀'],
+    ['Each of the books is a different color. 📚🌈', 'Cada uno de los libros es de un color diferente. 📚🌈'],
+    ['Each of them is a different color. 👥🌈', 'Cada uno de ellos es de un color diferente. 👥🌈'],
+    ['The students were each given a book. 📚👩‍🎓📗', 'A cada estudiante se le dio un libro. 📚👩‍🎓📗'],
+    ['These oranges cost 75 cents each. 🍊💰', 'Estas naranjas cuestan 75 centavos cada una. 🍊💰'],
+    ['Everyone enjoyed the party. 🎉😊', 'Todos disfrutaron la fiesta. 🎉😊'],
+    ['Sarah is invited to lots of parties and she goes to every one. 🎉👧🏼', 'Sarah está invitada a un montón de fiestas y va a cada una. 🎉👧🏼'],
+  ];
+  
+export default eachEvery;
