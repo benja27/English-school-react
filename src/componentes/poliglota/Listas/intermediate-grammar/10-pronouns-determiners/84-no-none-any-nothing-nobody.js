bf26695b8aa12cdd491e0598634d8b77fca9f8c0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-16 18:44:13 -0600

    Content: Add 84 No / none / any / Nothing / Nobody, etc.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/10-pronouns-determiners/84-no-none-any-nothing-nobody.js
@@ -0,0 +1,33 @@
+// 84 No/none/any Nothing/nobody, etc.
+
+const noNoneAnyNothing = [
+    ['We had to walk home because there was no bus. 🚶‍♂️', 'Tuvimos que caminar a casa porque no había autobús.'],
+    ['Sue will have no difficulty finding a job. 💼', 'Sue no tendrá dificultad para encontrar trabajo.'],
+    ['There were no stores open. 🏪', 'No había tiendas abiertas.'],
+    ['No reason was given for the change of plan. 🔄', 'No se dio ninguna razón para el cambio de plan.'],
+    ['How much money do you have? –None. 💰', '¿Cuánto dinero tienes? -Ninguno.'],
+    ['All the tickets have been sold. There are none left. 🎫', 'Todas las entradas han sido vendidas. No queda ninguna.'],
+    ['This money is all yours. None of it is mine. 💰', 'Este dinero es todo tuyo. Ninguno es mío.'],
+    ['None of the stores were open. 🏪', 'Ninguna de las tiendas estaba abierta.'],
+    ['Nobody came to visit me while I was in the hospital. 🏥', 'Nadie vino a visitarme mientras estaba en el hospital.'],
+    ['What happened? Nothing. 🤷‍♂️', '¿Qué pasó? Nada.'],
+    ['Where are you going? Nowhere. I’m staying here. 🏠', '¿Adónde vas? A ningún lado. Me quedo aquí.'],
+    ['The house is empty. There’s no one living there. 🏠', 'La casa está vacía. No vive nadie allí.'],
+    ['We had nothing to eat. 🍽️', 'No teníamos nada para comer.'],
+    ['I didn’t stay anything. 🤐', 'No dije nada.'],
+    ['Jane didn’t tell anybody about her plans. 🤐', 'Jane no le contó a nadie acerca de sus planes.'],
+    ['They don’t have anywhere to live. 🏠', 'No tienen dónde vivir.'],
+    ['I said nothing. 🤫', 'No dije nada.'],
+    ['Nobody tells me anything. 🤐', 'Nadie me dice nada.'],
+    ['There was no bus, so we walked home. 🚌', 'No había autobús, así que caminamos a casa.'],
+    ['You can take any bus. They all go downtown. 🚌', 'Puedes tomar cualquier autobús. Todos van al centro.'],
+    ['What do you want to eat? –Nothing. I’m not hungry. 🍽️', '¿Qué quieres comer? -Nada. No tengo hambre.'],
+    ['I’m so hungry I could eat anything. 🍽️', 'Tengo tanta hambre que podría comer cualquier cosa.'],
+    ['The exam was extremely difficult. Nobody passed. 📝', 'El examen fue extremadamente difícil. Nadie pasó.'],
+    ['The exam was very easy. Anybody could have passed. 📝', 'El examen fue muy fácil. Cualquiera podría haber pasado.'],
+    ['Nobody called, Did they? ☎️', 'Nadie llamó, ¿verdad?'],
+    ['No one did what I asked them to do. 📝', 'Nadie hizo lo que les pedí que hicieran.'],
+    ['Nobody in the class did their homework. 📚', 'Nadie en la clase hizo su tarea.'],
+  ];
+  
+export default noNoneAnyNothing;
