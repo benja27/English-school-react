aa8f635b5dd369caef7548ee1f00aa9458bc2e67
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-16 18:52:34 -0600

    Content: add 87 Both/ both of neither/ neither of, either/ either of.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/10-pronouns-determiners/87-both-neither.js
@@ -0,0 +1,32 @@
+// 87 Both / both of  neither / neither of   either/ either of
+
+const bothNeitherEither = [
+    ['Both restaurants are very good. 🍽️👌', 'Ambos restaurantes son muy buenos.'],
+    ['Neither restaurant is expensive. 💸🚫', 'Ningún restaurante es caro.'],
+    ['We can go to either restaurant. I don’t care. 🍽️🤷', 'Podemos ir a cualquiera de los dos restaurantes. No me importa.'],
+    ['Both of these restaurants are very good. 🍽️👌', 'Ambos de estos restaurantes son muy buenos.'],
+    ['Neither of the restaurants we went to was expensive. 💸🚫', 'Ninguno de los restaurantes a los que fuimos era caro.'],
+    ['I haven’t been to either of those restaurants. 🍽️🚫', 'No he ido a ninguno de esos restaurantes.'],
+    ['Both my parents are from Michigan. 👫🏞️', 'Ambos mis padres son de Michigan.'],
+    ['Can either of you speak Spanish? 🗣️🤔', '¿Puede alguno de ustedes hablar español?'],
+    ['I asked two people the way to the station, but neither of them knew. 🚶‍♂️🚶‍♀️🗺️', 'Le pregunté a dos personas cómo llegar a la estación, pero ninguna de ellas sabía.'],
+    ['Both of us were very tired. 👫😴', 'Ambos estábamos muy cansados.'],
+    ['Neither of the children wants to go to bed. 👦👧🛌', 'Ninguno de los niños quiere ir a la cama.'],
+    ['I couldn’t decide which of the two shirts to buy. I liked both. 👕🤔', 'No podía decidir qué camisa comprar. Me gustaban ambas.'],
+    ['Is your friend British or American? –Neither. She’s Australian. 🇬🇧🇺🇸🇦🇺', '¿Tu amiga es británica o estadounidense? -Ninguna. Es australiana.'],
+    ['Do you want tea or coffee? –Either. It doesn’t matter. ☕🍵', '¿Quieres té o café? -Cualquiera. No importa.'],
+    ['Both Ann and Tom were late. 👩‍🦰👨‍🦱⏰', 'Tanto Ann como Tom llegaron tarde.'],
+    ['I was both tired and hungry when I got home. 😴🍽️', 'Estaba tanto cansado como hambriento cuando llegué a casa.'],
+    ['Neither Liz nor Robin came to the party. 🎉🚫', 'Ni Liz ni Robin vinieron a la fiesta.'],
+    ['She said she would contact me, but she neither wrote nor called. 📱✉️', 'Dijo que me contactaría, pero ni escribió ni llamó.'],
+    ['I’m not sure where he’s from. He’s either Spanish or Italian. 🇪🇸🇮🇹', 'No estoy seguro de dónde es. Es español o italiano.'],
+    ['Either you apologize, or I’ll never speak to you again. 🙅‍♂️🗣️', 'O te disculpas, o nunca más te hablaré.'],
+    ['There are two good hotels here. You could stay at either of them. 🏨🏨', 'Hay dos buenos hoteles aquí. Podrías quedarte en cualquiera de ellos.'],
+    ['We tried two hotels. Neither of them had any rooms. 🏨🔍🚫', 'Probamos dos hoteles. Ninguno de ellos tenía habitaciones.'],
+    ['We tried 2 motels. Both of them were full. 🏨🔍🚫', 'Probamos 2 moteles. Ambos estaban llenos.'],
+    ['There are many good hotels here. You could stay at any of them. 🏨🏨🏨', 'Hay muchos buenos hoteles aquí. Podrías quedarte en cualquiera de ellos.'],
+    ['We tried a lot of hotels. None of them had any rooms. 🏨🔍🚫', 'Probamos muchos hoteles. Ninguno de ellos tenía habitaciones.'],
+    ['We tried a lot of motels. All of them were full. 🏨🔍🚫', 'Probamos muchos moteles. Todos estaban llenos.'],
+  ];  
+
+export default bothNeitherEither;
