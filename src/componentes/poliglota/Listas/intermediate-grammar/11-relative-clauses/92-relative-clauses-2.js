bb40f1d8d5f5b7613623c1b1ede05e899050cf8e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-17 22:30:01 -0600

    Content: Add 92 relative Clauses 3: Whose/whom/where

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/11-relative-clauses/92-relative-clauses-2.js
@@ -0,0 +1,23 @@
+// 92 Relative Clauses 3: Whose/whom/where
+
+const relativeClausesThree = [
+    ['We saw some people whose car had broken down. 🚗😔', 'Vimos a algunas personas cuyo coche se había averiado. 🚗😔'],
+    ['A widow is a woman whose husband is dead. 👩‍⚖️💔', 'Una viuda es una mujer cuyo esposo ha fallecido. 👩‍⚖️💔'],
+    ['What’s the name of the man whose car you borrowed? 🚗👨‍💼', '¿Cuál es el nombre del hombre cuyo coche tomaste prestado? 🚗👨‍💼'],
+    ['I met someone whose brother I went to school with. 👥🏫', 'Conocí a alguien cuyo hermano fue compañero de escuela mío. 👥🏫'],
+    ['I met a man who knows you. 👨‍🦱👋', 'Conocí a un hombre que te conoce. 👨‍🦱👋'],
+    ['I met a man whose sister knows you. 👨‍🦱👩‍🦱👋', 'Conocí a un hombre cuya hermana te conoce. 👨‍🦱👩‍🦱👋'],
+    ['The woman whom I wanted to see was away on vacation. 👩🏖️', 'La mujer a quien quería ver estaba de vacaciones. 👩🏖️'],
+    ['The people with whom I work are very nice. 👥👩‍💼', 'Las personas con quienes trabajo son muy amables. 👥👩‍💼'],
+    ['The woman I wanted to see. 👩👀', 'La mujer que quería ver. 👩👀'],
+    ['The woman who I wanted to see. 👩👀', 'La mujer a quien quería ver. 👩👀'],
+    ['The woman that I wanted to see. 👩👀', 'La mujer que quería ver. 👩👀'],
+    ['I recently went back to the town where I grew up. 🏠👦🔄', 'Recientemente volví al pueblo donde crecí. 🏠👦🔄'],
+    ['I would like to live in a place where there is plenty of sunshine. 🌞🏡', 'Me gustaría vivir en un lugar donde haya mucho sol. 🌞🏡'],
+    ['Do you remember the day that we went to the zoo? 🦁📅', '¿Recuerdas el día que fuimos al zoológico? 🦁📅'],
+    ['The last time that I saw her, she looked fine. 👩‍⚕️🕰️', 'La última vez que la vi, lucía bien. 👩‍⚕️🕰️'],
+    ['I haven’t seen them since the year that they got married. 👰🤵🗓️', 'No los he visto desde el año en que se casaron. 👰🤵🗓️'],
+    ['The reason I’m calling you is to ask your advice. 📞🤔', 'La razón por la que te llamo es para pedirte consejo. 📞🤔'],
+  ];
+  
+export default relativeClausesThree;
