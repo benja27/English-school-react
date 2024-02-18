a739d4bfe907c19d0a8d3a916dfefe3a9b0f8a85
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-17 22:22:43 -0600

    Content: Add 90 Relative Clauses 1: Clauses with who/that/which

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/11-relative-clauses/90-relative-clauses-1.js
@@ -0,0 +1,19 @@
+// 90 Relative Clauses 1: Clauses with who/that/which
+
+const relativeClausesOne = [
+    ['The woman who lives next door … 👩‍🦰🏠', 'La mujer que vive al lado… 👩‍🦰🏠'],
+    ['People who live in the country… 👨‍🌾🌳', 'Las personas que viven en el campo… 👨‍🌾🌳'],
+    ['An architect is someone who designs buildings. 👷‍♂️🏗️', 'Un arquitecto es alguien que diseña edificios. 👷‍♂️🏗️'],
+    ['What was the name of the person who called you? 📞👤', '¿Cuál era el nombre de la persona que te llamó? 📞👤'],
+    ['Anyone who wants to apply for the job must do so by Friday. 👨‍💼📅', 'Cualquiera que quiera solicitar el trabajo debe hacerlo antes del viernes. 👨‍💼📅'],
+    ['The woman who lives next door is a doctor. 👩‍⚕️💼', 'La mujer que vive al lado es doctora. 👩‍⚕️💼'],
+    ["I don’t like stories that have unhappy endings. 📖😔", "No me gustan las historias que tienen finales infelices. 📖😔"],
+    ['Barbara works for a company that makes furniture. 👩‍💼🪑', 'Barbara trabaja para una empresa que fabrica muebles. 👩‍💼🪑'],
+    ['The machine that broke down is working again now. 🛠️🔧', 'La máquina que se descompuso ahora está funcionando de nuevo. 🛠️🔧'],
+    ['What happened was my fault. 🤷‍♂️🚫', 'Lo que pasó fue mi culpa. 🤷‍♂️🚫'],
+    ['Everything that happened was my fault. 🔄🚫', 'Todo lo que pasó fue mi culpa. 🔄🚫'],
+    ['The machine that broke down is now working again. 🛠️🔄', 'La máquina que se descompuso ahora está funcionando de nuevo. 🛠️🔄'],
+    ["I’ve never spoken to the woman who lives next door. 🤐👩‍🦰", "Nunca he hablado con la mujer que vive al lado. 🤐👩‍🦰"],
+  ];
+  
+export default relativeClausesOne;
