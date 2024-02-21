1f4a1ec3e15197e22a1f746ccaa38a90ebf7e520
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-20 18:58:52 -0600

    Content: add 95 -ing and -ed Phrases (the woman talking to Tom, The boy injured in the accident)

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/11-relative-clauses/95-ing-ed-phrases.js
@@ -0,0 +1,21 @@
+// 95 –ing and –ed Phrases (the woman talking to Tom, the boy injured in the accident)
+
+const ingAndEdPhrases = [
+    ['Do you know the woman talking to Tom? 💬👩‍💼', '¿Conoces a la mujer que está hablando con Tom? 💬👩‍💼'],
+    ['The boy injured in the accident was taken to the hospital. 🚑👦', 'El niño herido en el accidente fue llevado al hospital. 🚑👦'],
+    ['Police investigating the crime are looking for three men. 👮‍♂️🔍', 'La policía que investiga el crimen está buscando a tres hombres. 👮‍♂️🔍'],
+    ['Who were those people waiting outside? 🧐👥', '¿Quiénes eran esas personas esperando afuera? 🧐👥'],
+    ['I was awakened by a bell ringing. 🔔😴', 'Me despertó el sonido de una campana. 🔔😴'],
+    ['The road connecting the two towns is very narrow. 🛣️🏘️', 'El camino que conecta los dos pueblos es muy estrecho. 🛣️🏘️'],
+    ['I have a large bedroom overlooking the garden. 🛏️🌳', 'Tengo un dormitorio grande con vista al jardín. 🛏️🌳'],
+    ['Can you think of the name of a flower beginning with “t”? 💭🌸', '¿Puedes pensar en el nombre de una flor que empiece con "t"? 💭🌸'],
+    ['George showed me some pictures painted by his father. 🖼️👨‍🎨', 'George me mostró algunas imágenes pintadas por su padre. 🖼️👨‍🎨'],
+    ['The police never found the money stolen in the robbery. 👮‍♂️💰', 'La policía nunca encontró el dinero robado en el robo. 👮‍♂️💰'],
+    ['Most of the goods made in this factory are exported. 🏭📦', 'La mayoría de los productos fabricados en esta fábrica se exportan. 🏭📦'],
+    ['We’ve eaten almost all the chocolates. There are only a few left. 🍫👀', 'Nos hemos comido casi todos los chocolates. Solo quedan unos pocos. 🍫👀'],
+    ['There were some children swimming in the river. 🏊‍♂️👦👧', 'Había algunos niños nadando en el río. 🏊‍♂️👦👧'],
+    ['Is there anybody waiting? 🤔🕵️‍♂️', '¿Hay alguien esperando? 🤔🕵️‍♂️'],
+    ['There was a big red car parked outside the house. 🚗🏠', 'Había un coche rojo grande estacionado afuera de la casa. 🚗🏠'],
+  ];  
+
+export default ingAndEdPhrases;
