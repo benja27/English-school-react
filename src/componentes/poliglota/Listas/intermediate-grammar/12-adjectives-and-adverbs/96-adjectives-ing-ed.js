44c61224948efda7b3f8a237722322e6838f626c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-21 22:36:30 -0600

    Content: add 96 Adjectives Ending in -ing and -ed (boring/bored, etc.)

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/12-adjectives-and-adverbs/96-adjectives-ing-ed.js
@@ -0,0 +1,21 @@
+// 96 Adjectives Ending in –ing and –ed (boring/bored, etc.)
+
+const adjectivesIngEd = [
+    ['Jane is bored because her job is boring. 😴😑', 'Jane está aburrida porque su trabajo es aburrido. 😴😑'],
+    ['Jane’s job is boring, so Jane is bored. 😑😩', 'El trabajo de Jane es aburrido, así que Jane está aburrida. 😑😩'],
+    ['George always talks about the same things. He’s really boring. 🗣️😴', 'George siempre habla de las mismas cosas. Es realmente aburrido. 🗣️😴'],
+    ['My job is interesting. 😃👩‍💼', 'Mi trabajo es interesante. 😃👩‍💼'],
+    ['My job is tiring. 😩💼', 'Mi trabajo es agotador. 😩💼'],
+    ['My job is satisfying. 😊👍', 'Mi trabajo es satisfactorio. 😊👍'],
+    ['I’m not interested in my job anymore. 🙅‍♀️😕', 'Ya no estoy interesado en mi trabajo. 🙅‍♀️😕'],
+    ['I get tired doing my job. 😫💼', 'Me canso haciendo mi trabajo. 😫💼'],
+    ['I’m not satisfied with my job. 😕❌', 'No estoy satisfecho con mi trabajo. 😕❌'],
+    ['Julia thinks politics is interesting. 🗳️🤔', 'Julia piensa que la política es interesante. 🗳️🤔'],
+    ['Julia is interested in politics. 🤓🗳️', 'Julia está interesada en la política. 🤓🗳️'],
+    ['Did you meet anyone interesting at the party? 🎉🤔', '¿Conociste a alguien interesante en la fiesta? 🎉🤔'],
+    ['Are you interested in buying a car? I’m trying to sell mine. 🚗💰', '¿Estás interesado en comprar un coche? Estoy tratando de vender el mío. 🚗💰'],
+    ['It was surprising that he passed the exam. 😲📚', 'Fue sorprendente que aprobara el examen. 😲📚'],
+    ['Everybody was surprised that he passed the exam. 😱📚', 'Todos se sorprendieron de que pasara el examen. 😱📚'],
+  ];
+  
+export default adjectivesIngEd;
