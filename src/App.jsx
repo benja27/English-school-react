1a190fdd03ca4addde3a881d6b7eff3c6a0aa606
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-28 16:51:55 -0600

    feat/SplashScreen set state and conditinally render the splash screen.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-
-
 import Navbar from './componentes/general/Navbar/Navbar'
 import useStore from './componentes/Zustand/State'
 import MidElement from './componentes/general/ElementoCentral.jsx/MidElement'
 import Notepad from './componentes/general/Notepad'
+import SplashScreen from './pages/SplashScreen';
+import { useState } from 'react';
 
 
 function App() {
   const {visible} = useStore()
   console.log(visible)
+  const [splashVisibility, setSplashVisibility] = useState(true);
 
   return (
     <div>
       <Navbar />
       <Notepad/>
+      {splashVisibility && (
+        <SplashScreen
+          visibility={splashVisibility}
+          setVisibility={setSplashVisibility}
+        />
+      )}
       <MidElement/>
       {/* <BookGrama/>  */}
     </div>
-
-    
   )
 }
 
