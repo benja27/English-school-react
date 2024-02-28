e4861abd20af155c644dec89096e458c7ea02e1c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-27 21:01:16 -0600

    Content: add 108 Still, yet, and already Anymore / any lnoger / no longer.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/12-adjectives-and-adverbs/108-still-yet-already-anymore.js
@@ -0,0 +1,22 @@
+// 108 Still, yet, and already   Anymore / any longer / no longer 
+
+const stillYetAlreadyAnymore = [
+    ['It’s 10:00 and Joe is still in bed. ⏰😴', 'Son las 10:00 y Joe todavía está en la cama. ⏰😴'],
+    ['When I went to bed, Chris was still working. 🌙💼', 'Cuando me fui a la cama, Chris todavía estaba trabajando. 🌙💼'],
+    ['Do you still want to go to the party, or have you changed your mind? 💃🤔', '¿Todavía quieres ir a la fiesta o has cambiado de opinión? 💃🤔'],
+    ['Lucy doesn’t work here anymore. She left last month. 👩‍💼👋', 'Lucy ya no trabaja aquí. Se fue el mes pasado. 👩‍💼👋'],
+    ['We used to be good friends, but we aren’t anymore/any longer. 👫❌', 'Solíamos ser buenos amigos, pero ya no lo somos. 👫❌'],
+    ['Lucy no longer works here. 👩‍💼❌', 'Lucy ya no trabaja aquí. 👩‍💼❌'],
+    ['We are no longer friends. 👫❌', 'Ya no somos amigos. 👫❌'],
+    ['Sally still works here, but Ann doesn’t work here anymore. 👩‍💼👩‍💼❌', 'Sally todavía trabaja aquí, pero Ann ya no trabaja aquí. 👩‍💼👩‍💼❌'],
+    ['It’s 10:00 and Joe isn’t here yet. ⏰🤷‍♂️', 'Son las 10:00 y Joe todavía no está aquí. ⏰🤷‍♂️'],
+    ['Have you met your new neighbors yet? 🏡👋', '¿Ya has conocido a tus nuevos vecinos? 🏡👋'],
+    ['Mike lost his job six months ago and is still unemployed. 👨‍💼❌', 'Mike perdió su trabajo hace seis meses y todavía está desempleado. 👨‍💼❌'],
+    ['Has it stopped raining yet? 🌧️🛑', '¿Ya dejó de llover? 🌧️🛑'],
+    ['She said she would be here an hour ago, and she still hasn’t come. 👩⏳', 'Dijo que estaría aquí hace una hora y todavía no ha venido. 👩⏳'],
+    ['What time is Sue leaving? –She has already left. ⌚🚪', '¿A qué hora se va Sue? - Ya se fue. ⌚🚪'],
+    ['Should I tell Joe what happened, or does he already know? 🤔🗣️', '¿Debería decirle a Joe lo que pasó o ya lo sabe? 🤔🗣️'],
+    ['I’ve just had lunch, and I’m already hungry. 🍽️😋', 'Acabo de almorzar y ya tengo hambre. 🍽️😋']
+  ];  
+
+export default stillYetAlreadyAnymore;
