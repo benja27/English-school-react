1b148010e64aabf2b8c6e57841cb90cff280f3a4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-27 21:03:41 -0600

    Content: add 109 Even.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/12-adjectives-and-adverbs/109-even.js
@@ -0,0 +1,21 @@
+// 109 Even
+
+const even = [
+    ['She has a TV in every room of the house, even the bathroom. 📺🏠', 'Ella tiene un televisor en cada habitación de la casa, incluso en el baño. 📺🏠'],
+    ['These pictures are really awful. Even I could take better pictures than these. 🖼️😬', 'Estas fotos son realmente horribles. Incluso yo podría tomar mejores fotos que estas. 🖼️😬'],
+    ['He always wears a coat, even in hot weather. 🧥🌞', 'Siempre lleva un abrigo, incluso cuando hace calor. 🧥🌞'],
+    ['Nobody would help her, not even her best friend. 🙅‍♀️👭', 'Nadie la ayudaría, ni siquiera su mejor amiga. 🙅‍♀️👭'],
+    ['Sue has traveled all over the world. She has even been to the Antarctic. 🌍🐧', 'Sue ha viajado por todo el mundo. Incluso ha estado en la Antártida. 🌍🐧'],
+    ['They are very rich. They even have their own private jet. 💰✈️', 'Son muy ricos. Incluso tienen su propio jet privado. 💰✈️'],
+    ['I can’t cook. I can’t even boil an egg. 🍳😅', 'No sé cocinar. No puedo ni siquiera hervir un huevo. 🍳😅'],
+    ['They weren’t very friendly to us. They didn’t even say hello. 😕👋', 'No fueron muy amables con nosotros. Ni siquiera dijeron hola. 😕👋'],
+    ['I got up very early, but Jack got up even earlier. ⏰🌅', 'Me levanté muy temprano, pero Jack se levantó aún más temprano. ⏰🌅'],
+    ['I knew I didn’t have much money, but I have even less than I thought. 💸📉', 'Sabía que no tenía mucho dinero, pero tengo incluso menos de lo que pensaba. 💸📉'],
+    ['Even though she can’t drive, she bought a car. 🚗🤷‍♀️', 'Aunque no puede conducir, compró un coche. 🚗🤷‍♀️'],
+    ['He never shouts, even when he’s angry. 🤬🤐', 'Nunca grita, incluso cuando está enojado. 🤬🤐'],
+    ['I can’t reach the shelf even if I stand on a chair. 🪑🤨', 'No puedo alcanzar el estante incluso si me paro en una silla. 🪑🤨'],
+    ['We’re going to the beach tomorrow. It doesn’t matter what the weather is like. We’re going even if it’s raining. 🏖️🌧️', 'Vamos a la playa mañana. No importa cómo esté el clima. Iremos incluso si está lloviendo. 🏖️🌧️'],
+    ['We want to go to the beach tomorrow, but we won’t go if it’s raining. 🏖️☔', 'Queremos ir a la playa mañana, pero no iremos si está lloviendo. 🏖️☔']
+  ];  
+
+export default even;
