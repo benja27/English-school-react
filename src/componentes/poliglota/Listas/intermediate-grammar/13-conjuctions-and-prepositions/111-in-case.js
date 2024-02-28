445606c2297c8e07fdeafd3b23a9df3edbd1097e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-28 16:11:05 -0600

    Content: add 111 In case.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/13-conjuctions-and-prepositions/111-in-case.js
@@ -0,0 +1,17 @@
+// 111 in case
+
+const inCase = [
+    ['Your car should have a spare tire in case you have a flat tire. 🚗🔧', 'Tu coche debería tener una llanta de repuesto en caso de que tengas un neumático pinchado. 🚗🔧'],
+    ['I’ll leave my cell phone on in case Jane calls. 📱🔔', 'Dejaré mi teléfono celular encendido en caso de que Jane llame. 📱🔔'],
+    ['I’ll draw a map for you in case you can’t find our house. 🗺️🏠', 'Te dibujaré un mapa en caso de que no puedas encontrar nuestra casa. 🗺️🏠'],
+    ['I’ll remind them about the meeting in case they’ve forgotten. ⏰🗓️', 'Les recordaré sobre la reunión en caso de que se les haya olvidado. ⏰🗓️'],
+    ['I don’t think it will rain, but I’ll take an umbrella just in case. ☂️☁️', 'No creo que llueva, pero llevaré un paraguas por si acaso. ☂️☁️'],
+    ['We’ll buy some more food in case Tom comes. 🛒👨‍🍳', 'Compraremos más comida en caso de que Tom venga. 🛒👨‍🍳'],
+    ['I’ll give you my phone number in case you need to contact me. 📞📝', 'Te daré mi número de teléfono en caso de que necesites contactarme. 📞📝'],
+    ['I left my phone on in case Jane called. 📲🔔', 'Dejé mi teléfono encendido en caso de que Jane llamara. 📲🔔'],
+    ['I drew a map for Sarah in case she couldn’t find the house. 🗺️🏠', 'Dibujé un mapa para Sarah en caso de que no pudiera encontrar la casa. 🗺️🏠'],
+    ['In case of fire, please leave the building as quickly as possible. 🔥🏢', 'En caso de incendio, por favor salga del edificio lo más rápido posible. 🔥🏢'],
+    ['In case of emergency, call this number. 🚨📞', 'En caso de emergencia, llame a este número. 🚨📞'],
+  ];  
+
+export default inCase;
