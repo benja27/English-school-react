83fc963a5999b248423a244dc8ac70b390c7535c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-29 20:28:10 -0600

    Content: add 112 Unless As long ass Provided/providing.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/13-conjuctions-and-prepositions/112-unless-as-long-provided-ing.js
@@ -0,0 +1,17 @@
+// 112 Unless  As long ass   Provided/providing
+
+const unlessAsLongProvided = [
+    ['I’ll see you tomorrow unless I have to work late. 😊⏰', 'Te veré mañana a menos que tenga que trabajar hasta tarde. 😊⏰'],
+    ['There are no buses to the beach. Unless you have a car. 🚌🏖️', 'No hay autobuses a la playa. A menos que tengas un coche. 🚌🏖️'],
+    ['Should I tell Liz what happened? –Not unless she asks you. 🤔❓', '¿Debería contarle a Liz lo que pasó? -No a menos que ella te lo pregunte. 🤔❓'],
+    ['Sally hates to complain. She wouldn’t complain about something unless it was really bad. 😒👎', 'A Sally no le gusta quejarse. No se quejaría de algo a menos que fuera realmente malo. 😒👎'],
+    ['We can take a taxi to the restaurant, unless you’d prefer to walk. 🚖🚶', 'Podemos tomar un taxi al restaurante, a menos que prefieras caminar. 🚖🚶'],
+    ['Unless we leave now, we’ll be late. 🕒🏃‍♂️', 'A menos que salgamos ahora, llegaremos tarde. 🕒🏃‍♂️'],
+    ['You can borrow my car as long as you promise not to drive too fast. 🚗🤝', 'Puedes tomar prestado mi coche siempre y cuando prometas no conducir demasiado rápido. 🚗🤝'],
+    ['Traveling by car is convenient provided you have somewhere to park. 🚗🅿️', 'Viajar en coche es conveniente siempre y cuando tengas un lugar donde estacionar. 🚗🅿️'],
+    ['Providing the room is clean, I don’t care which hotel we stay at. 🏨🧹', 'Si la habitación está limpia, no me importa en qué hotel nos alojemos. 🏨🧹'],
+    ['I’m not going out unless it stops raining. ☔🙅‍♂️', 'No saldré a menos que deje de llover. ☔🙅‍♂️'],
+    ['Providing the weather is good, we’re going on a picnic. ☀️🧺', 'Si el clima es bueno, iremos de picnic. ☀️🧺'],
+  ];  
+
+export default unlessAsLongProvided;
