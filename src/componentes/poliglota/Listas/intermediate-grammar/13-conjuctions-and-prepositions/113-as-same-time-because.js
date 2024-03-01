d82016389eafeaa9900a96a8e224062239d34339
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-29 20:30:34 -0600

    Content: add 113 As(= at the same time) and as (=because).

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/13-conjuctions-and-prepositions/113-as-same-time-because.js
@@ -0,0 +1,21 @@
+// 113 As (= at the same time) and as (=because)
+
+const asSameBecause = [
+    ['We all waved goodbye to Liz as she drove away. 👋🚗', 'Todos le saludamos con la mano a Liz mientras se alejaba conduciendo. 👋🚗'],
+    ['I watched her as she opened the letter. 👀💌', 'La observé mientras abría la carta. 👀💌'],
+    ['As I walked along the street, I looked in the store windows. 🚶‍♂️🏙️', 'Mientras caminaba por la calle, miraba las vitrinas de las tiendas. 🚶‍♂️🏙️'],
+    ['Can you turn off the light as you go out, please? 💡🚪', '¿Puedes apagar la luz al salir, por favor? 💡🚪'],
+    ['Jill slipped as she was getting off the bus. 😧🚌', 'Jill resbaló mientras bajaba del autobús. 😧🚌'],
+    ['We met Paul as we were leaving the hotel. 👋🏨', 'Nos encontramos con Paul mientras salíamos del hotel. 👋🏨'],
+    ['Just as I sat down, the phone rang. ☎️🪑', 'Justo cuando me senté, sonó el teléfono. ☎️🪑'],
+    ['I had to leave just as the conversation was getting interesting. 🗣️👋', 'Tuve que irme justo cuando la conversación se estaba poniendo interesante. 🗣️👋'],
+    ['As the day went on, the weather got worse. 🌧️⏳', 'A medida que avanzaba el día, el clima empeoraba. 🌧️⏳'],
+    ['I began to enjoy the job more as I got used to it. 😊💼', 'Comencé a disfrutar más del trabajo a medida que me acostumbraba. 😊💼'],
+    ['As we walked home, we talked about what we would have for dinner. 🚶‍♂️🍽️', 'Mientras caminábamos a casa, hablamos sobre lo que íbamos a cenar. 🚶‍♂️🍽️'],
+    ['As it was a national holiday last Thursday, all the banks were closed. 📅🏦', 'Como fue un día festivo nacional el jueves pasado, todos los bancos estaban cerrados. 📅🏦'],
+    ['The thief was difficult to identify as he was wearing a mask during the robbery. 👤🎭', 'Fue difícil identificar al ladrón porque llevaba una máscara durante el robo. 👤🎭'],
+    ['Since it was a national holiday last Thursday, all the banks were closed. 📅🏦', 'Dado que fue un día festivo nacional el jueves pasado, todos los bancos estaban cerrados. 📅🏦'],
+    ['As they lived near us, we used to see them pretty often. 🏠👀', 'Como vivían cerca de nosotros, solíamos verlos bastante seguido. 🏠👀'],
+  ];
+  
+export default asSameBecause;
