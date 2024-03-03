afdbf0ebbb4372b83637933aafe42012d61764c6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-02 18:13:47 -0600

    feat/SplashScreen: Resize Welcome Text.

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -10,7 +10,7 @@ const SplashScreen = ({ visibility, setVisibility }) => {
     <section className='absolute h-screen w-screen bg-slate-300 flex items-center justify-center
     flex-col gap-2
     '>
-      <h2>Welcome to English School!</h2>
+      <h2 className='text-4xl font-bold p-2 m-2'>Welcome to English School!</h2>
       <p>
         We are glad to have you here. We hope you enjoy your stay and learn a
         lot of English.
