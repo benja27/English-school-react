542bc2298b6947ca7eb690dd444447e3a41cb4f0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-04 21:47:16 -0600

    Content: Add 117 By and until, By the time ...

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/13-conjuctions-and-prepositions/117-by-until.js
@@ -0,0 +1,21 @@
+// 117 By and until  By the time…
+
+const byUntil = [
+    ['I sent the letter to them today, so they should receive it by Monday. 📬📅', 'Hoy envié la carta a ellos, así que deberían recibirla para el lunes. 📬📅'],
+    ['We’d better hurry. We have to be home by 5:00. ⏰🏠', 'Será mejor que nos apuremos. Debemos estar en casa para las 5:00. ⏰🏠'],
+    ['Where’s Sue? She should be here by now. 🤔🕒', '¿Dónde está Sue? Debería estar aquí para ahora. 🤔🕒'],
+    ['Shall we go now? –No, let’s wait until it stops raining. 🌧️🚶', '¿Deberíamos ir ahora? –No, esperemos hasta que pare de llover. 🌧️🚶'],
+    ['I couldn’t get up this morning. I stayed in bed until half past ten. 😴⏰', 'No pude levantarme esta mañana. Me quedé en la cama hasta las diez y media. 😴⏰'],
+    ['Fred will be away until Monday. 🛫📅', 'Fred estará fuera hasta el lunes. 🛫📅'],
+    ['Fred will be back by Monday. 🛬📅', 'Fred estará de vuelta para el lunes. 🛬📅'],
+    ['I’ll be working until 11:30. 💼⏰', 'Estaré trabajando hasta las 11:30. 💼⏰'],
+    ['I’ll have finished my work by 11:30. 💼🕦', 'Habré terminado mi trabajo para las 11:30. 💼🕦'],
+    ['It’s too late to go to the bank now. By the time we get there, it will be closed. 🏦🕒', 'Es demasiado tarde para ir al banco ahora. Para cuando lleguemos, estará cerrado. 🏦🕒'],
+    ['Our vacation ends tomorrow. So by the time you receive this postcard, I’ll be back home. 🏖️📆', 'Nuestras vacaciones terminan mañana. Así que para cuando recibas esta postal, estaré de vuelta en casa. 🏖️📆'],
+    ['Hurry up! By the time we get to the theater, the play will already have started. ⏳🎭', '¡Apúrate! Para cuando lleguemos al teatro, la obra ya habrá comenzado. ⏳🎭'],
+    ['Karen’s car broke down on the way to the party last night. By the time she arrived, most of the other guests had left. 🚗🎉', 'El carro de Karen se averió en el camino a la fiesta anoche. Para cuando ella llegó, la mayoría de los otros invitados ya se habían ido. 🚗🎉'],
+    ['I had a lot of work to do last night. I was very tired by the time I finished. 💼😴', 'Tenía mucho trabajo que hacer anoche. Estaba muy cansado para cuando terminé. 💼😴'],
+    ['We went to the theater last night. It took us a long time to find a place to park. By the time we got to the theater, the play had already started. 🚗🎭', 'Fuimos al teatro anoche. Nos llevó mucho tiempo encontrar un lugar para estacionar. Para cuando llegamos al teatro, la obra ya había comenzado. 🚗🎭'],
+  ];  
+
+export default byUntil;
