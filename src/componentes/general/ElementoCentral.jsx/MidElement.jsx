17fe3cb805a9c3420032b5c0cb01f019f1a05edc
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-04 22:23:44 -0600

    refactor/html(MidElement.jsx): Add filter-panel ID for better readability.

--- src/componentes/general/ElementoCentral.jsx/MidElement.jsx
+++ src/componentes/general/ElementoCentral.jsx/MidElement.jsx
@@ -15,7 +15,9 @@ function MidElement() {
       id="panels-container"
       className="flex flex-col sm:flex-row items-center sm:items-start bg-slate-300 min-h-screen  gap-2 justify-around items-start p-7">
       <PreguntayRespuesta/>
-      <div className=" w-full flex flex-col" >
+      <div 
+        id="filter-panel"
+        className=" w-full flex flex-col" >
           <BookGrama/>
           <Casa/>
           <div>
