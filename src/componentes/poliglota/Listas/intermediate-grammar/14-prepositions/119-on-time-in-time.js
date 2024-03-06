fe3d75d4ad1ac22f06f7bced1ac6d33c98694459
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-05 21:04:13 -0600

    Content: add 119 On time and in time At the end and in the end.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/119-on-time-in-time.js
@@ -0,0 +1,25 @@
+// 119 On time and in time   At the end and in the end
+
+const onInTimeEnd = [
+    ['The 11:45 train left on time. ⏰🚆', 'El tren de las 11:45 salió a tiempo. ⏰🚆'],
+    ['I’ll meet you at 7:30. —OK, but please be on time. 🕢🤝', 'Te veré a las 7:30. —OK, pero por favor, sé puntual. 🕢🤝'],
+    ['The conference was well organized. everything began and ended on time. 📅👍', 'La conferencia estuvo bien organizada. Todo comenzó y terminó a tiempo. 📅👍'],
+    ['Be on time. Don’t be late. ⏰🚫', 'Sé puntual. No llegues tarde. ⏰🚫'],
+    ['Will you be home in time for dinner? 🍽️🕢', '¿Estarás en casa a tiempo para la cena? 🍽️🕢'],
+    ['I’ve sent Jill a birthday present. I hope it arrives in time (for her birthday). 🎁📅', 'Le envié a Jill un regalo de cumpleaños. Espero que llegue a tiempo (para su cumpleaños). 🎁📅'],
+    ['I’m in a hurry. I want to be home in time to see the game on television. ⏳📺', 'Tengo prisa. Quiero estar en casa a tiempo para ver el partido en televisión. ⏳📺'],
+    ['I got home too late to see the game on television. 🕰️📺', 'Llegué a casa demasiado tarde para ver el partido en televisión. 🕰️📺'],
+    ['We got to the station just in time for our train. ⏰🚉', 'Llegamos a la estación justo a tiempo para nuestro tren. ⏰🚉'],
+    ['A child ran into the street in front of the car. I managed to stop just in time. 🚗🚸', 'Un niño corrió hacia la calle delante del coche. Logré detenerme justo a tiempo. 🚗🚸'],
+    ['I’m going away at the end of January. 📅🔚', 'Me voy a finales de enero. 📅🔚'],
+    ['I’m going away at the end of the month. 📅🔚', 'Me voy a fin de mes. 📅🔚'],
+    ['At the end of the concert, there was great applause. 🎶👏', 'Al final del concierto, hubo grandes aplausos. 🎶👏'],
+    ['The players shook hands at the end of the game. 🤝⚽', 'Los jugadores se estrecharon la mano al final del partido. 🤝⚽'],
+    ['I’m going away at the beginning of January. 📅🔜', 'Me voy a principios de enero. 📅🔜'],
+    ['We had a lot of problems with our car. We sold it in the end. 🚗💰', 'Tuvimos muchos problemas con nuestro coche. Lo vendimos al final. 🚗💰'],
+    ['He got angrier and angrier. In the end he just walked out of the room. 😠🚪', 'Se puso más y más enojado. Al final, simplemente salió de la habitación. 😠🚪'],
+    ['Alan couldn’t decide where to go on vacation. He didn’t go anywhere in the end. 🤔✈️', 'Alan no podía decidir a dónde ir de vacaciones. Al final, no fue a ningún lado. 🤔✈️'],
+    ['At first we didn’t get along very well, but in the end we became good friends. 👫🤝', 'Al principio no nos llevábamos muy bien, pero al final nos convertimos en buenos amigos. 👫🤝'],
+  ];  
+
+export default onInTimeEnd;
