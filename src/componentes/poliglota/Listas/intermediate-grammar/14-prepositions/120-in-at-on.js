5dc293869f81026d5fd079a65598a8f9b534d4c6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-06 18:26:39 -0600

    Content: add 120 In/at/on (Position) 1.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/120-in-at-on.js
@@ -0,0 +1,28 @@
+// 120 In/at/on (Position) 1
+
+const inAtOn = [
+    ['There’s somebody in the room 👤🚪', 'Hay alguien en la habitación 👤🚪'],
+    ['What do you have in your hand? 🤲🤔', '¿Qué tienes en la mano? 🤲🤔'],
+    ['When we were in Chile, we spent a few days in Santiago. 🇨🇱🌆', 'Cuando estábamos en Chile, pasamos unos días en Santiago. 🇨🇱🌆'],
+    ['I have a friend who lives in a small village in the mountains. 👫🏞️', 'Tengo un amigo que vive en un pequeño pueblo en las montañas. 👫🏞️'],
+    ['There were some people swimming in the pool. 🏊‍♂️🏊‍♀️', 'Había algunas personas nadando en la piscina. 🏊‍♂️🏊‍♀️'],
+    ['Do you know that man standing at the bus stop? 🚶‍♂️🚌', '¿Conoces al hombre que está parado en la parada de autobús? 🚶‍♂️🚌'],
+    ['Turn left at the traffic light. ⬅️🚦', 'Gira a la izquierda en el semáforo. ⬅️🚦'],
+    ['We have to get off the bus at the next stop. 🚌🛑', 'Tenemos que bajarnos del autobús en la próxima parada. 🚌🛑'],
+    ['When you leave the hotel, please leave your key at the front desk. 🛎️🔑', 'Cuando salgas del hotel, por favor deja tu llave en la recepción. 🛎️🔑'],
+    ['I sat on the floor. 🪑🔽', 'Me senté en el suelo. 🪑🔽'],
+    ['There’s a dirty mark on the wall. 💧🏠', 'Hay una marca sucia en la pared. 💧🏠'],
+    ['Have you seen the notice on the bulletin board? 📌👀', '¿Has visto el aviso en el tablón de anuncios? 📌👀'],
+    ['You’ll find the listings of TV programs on page 7 (of the newspaper). 📺📰', 'Encontrarás los listados de programas de televisión en la página 7 (del periódico). 📺📰'],
+    ['The hotel is on a small island in the middle of the lake. 🏝️🏞️', 'El hotel está en una pequeña isla en medio del lago. 🏝️🏞️'],
+    ['There were a lot of people in the store. It was very crowded. 👥🏬', 'Había mucha gente en la tienda. Estaba muy llena. 👥🏬'],
+    ['Go along this road, then turn left at the store. 🛣️⬅️', 'Sigue esta carretera, luego gira a la izquierda en la tienda. 🛣️⬅️'],
+    ['I’ll meet you in the hotel lobby. 🏨🤝', 'Te veré en el vestíbulo del hotel. 🏨🤝'],
+    ['I’ll meet you at the entrance to the hotel. 🚪🤝', 'Te veré en la entrada del hotel. 🚪🤝'],
+    ['There is some water in the bottle. 💧🧴', 'Hay agua en la botella. 💧🧴'],
+    ['There is a label on the bottle. 🏷️🧴', 'Hay una etiqueta en la botella. 🏷️🧴'],
+    ['There is somebody at the door. Should I go and see who it is? 👤🚪', 'Hay alguien en la puerta. ¿Debería ir a ver quién es? 👤🚪'],
+    ['There is a sign on the door. It says “Do not disturb.” 🚪🚫', 'Hay un letrero en la puerta. Dice "No molestar". 🚪🚫'],
+  ];
+  
+export default inAtOn;
