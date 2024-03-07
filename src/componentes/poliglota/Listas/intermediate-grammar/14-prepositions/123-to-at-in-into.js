d33aba41fd2c60b0bfaf0693934fb1d5663a321e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-07 16:51:38 -0600

    Content: Add 123 To/at/in/into.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/123-to-at-in-into.js
@@ -0,0 +1,28 @@
+// 123 To/at/in/into
+
+const toAtInInto = [
+    ['When are your friends going back to Italy? 🇮🇹⏰', '¿Cuándo vuelven tus amigos a Italia? 🇮🇹⏰'],
+    ['Three people were injured in the accident and taken to the hospital. 🚑🏥', 'Tres personas resultaron heridas en el accidente y fueron llevadas al hospital. 🚑🏥'],
+    ['Welcome to our country! 🌍🎉', '¡Bienvenidos a nuestro país! 🌍🎉'],
+    ['Did you enjoy your trip to Paris? ✈️😊', '¿Disfrutaste tu viaje a París? ✈️😊'],
+    ['Did you enjoy your visit to the zoo? 🦁🐵', '¿Disfrutaste tu visita al zoológico? 🦁🐵'],
+    ['They are going to France. 🇫🇷✈️', 'Ellos van a Francia. 🇫🇷✈️'],
+    ['They live in France. 🇫🇷🏠', 'Ellos viven en Francia. 🇫🇷🏠'],
+    ['Can you come to the party? 🎉🤔', '¿Puedes venir a la fiesta? 🎉🤔'],
+    ['I’ll see you at the party. 🎈👀', 'Nos vemos en la fiesta. 🎈👀'],
+    ['I’ve been to Italy four times, but I’ve never been to Rome. 🇮🇹✈️', 'He estado en Italia cuatro veces, pero nunca he estado en Roma. 🇮🇹✈️'],
+    ['Amanda has never been to a hockey game in her life. 🏒😯', 'Amanda nunca ha ido a un partido de hockey en su vida. 🏒😯'],
+    ['What time did they get to London? 🕰️🇬🇧', '¿A qué hora llegaron a Londres? 🕰️🇬🇧'],
+    ['They arrived in Rio de Janeiro a week ago. 🌴🛬', 'Llegaron a Río de Janeiro hace una semana. 🌴🛬'],
+    ['When did they arrive at the hotel? 🏨⏰', '¿Cuándo llegaron al hotel? 🏨⏰'],
+    ['I’m tired. Let’s go home now. 🏠😴', 'Estoy cansado. Vamos a casa ahora. 🏠😴'],
+    ['I met Linda on my way home. 🚶‍♂️👋', 'Conocí a Linda camino a casa. 🚶‍♂️👋'],
+    ['I opened the door, went into the room, and sat down. 🚪💺', 'Abrí la puerta, entré en la habitación y me senté. 🚪💺'],
+    ['A bird flew into the kitchen through the window. 🐦🪟', 'Un pájaro voló hacia la cocina por la ventana. 🐦🪟'],
+    ['She got in the car and drove away. 🚗🛣️', 'Ella se subió al coche y se fue. 🚗🛣️'],
+    ['I read the letter and put it back in the envelope. 💌📖', 'Leí la carta y la metí de nuevo en el sobre. 💌📖'],
+    ['She got out of the car and went into a shop. 🚗🛍️', 'Ella se bajó del coche y entró en una tienda. 🚗🛍️'],
+    ['She got on the bus and I never saw her again. 🚌👋', 'Se subió al autobús y nunca más la vi. 🚌👋'],
+  ];  
+
+export default toAtInInto;
