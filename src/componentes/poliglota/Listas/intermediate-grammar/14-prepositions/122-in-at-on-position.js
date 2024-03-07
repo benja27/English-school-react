349d82df65b4062b21346781d6758756078b5823
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-07 16:48:29 -0600

    Content: add 122 in/at/on (Position) 3

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/122-in-at-on-position.js
@@ -0,0 +1,25 @@
+// 122 In/at/on (Position) 3
+
+const inAtOnPosition = [
+    ["I’ll be at work until 5:30, but I’ll be at home all evening. 🏢🕠", "Estaré en el trabajo hasta las 5:30, pero estaré en casa toda la tarde. 🏢🕠"],
+    ["You can stop by anytime. I’ll be home all evening. 🏠⏰", "Puedes pasar cuando quieras. Estaré en casa toda la tarde. 🏠⏰"],
+    ["Ann’s mother is in the hospital. 👩‍⚕️🏥", "La madre de Ann está en el hospital. 👩‍⚕️🏥"],
+    ["Kim is not living at home. She’s away at college. 🎓🏠", "Kim no está viviendo en casa. Está en la universidad. 🎓🏠"],
+    ["Amy works at a bank and her brother is in medical school. 💼👩‍🔬", "Amy trabaja en un banco y su hermano está en la escuela de medicina. 💼👩‍🔬"],
+    ["Were there many people at the party / at the meeting / at the wedding? 🎉👥", "¿Había mucha gente en la fiesta / en la reunión / en la boda? 🎉👥"],
+    ["I saw Steve at a tennis match / at a concert on Saturday. 🎾🎵", "Vi a Steve en un partido de tenis / en un concierto el sábado. 🎾🎵"],
+    ["We went to a concert at Lincoln Center. 🎶🏛️", "Fuimos a un concierto en el Lincoln Center. 🎶🏛️"],
+    ["The meeting took place at the company’s headquarters in New York. 🏢🗽", "La reunión tuvo lugar en la sede de la empresa en Nueva York. 🏢🗽"],
+    ["Don’t meet at the station. I can get a taxi. 🚉🚖", "No te encuentres en la estación. Puedo tomar un taxi. 🚉🚖"],
+    ["I was at Sue’s house last night. 🏠🌙", "Estuve en la casa de Sue anoche. 🏠🌙"],
+    ["We had dinner at the hotel. 🍽️🏨", "Cenamos en el hotel. 🍽️🏨"],
+    ["All the rooms in the hotel have air conditioning. ❄️🏨", "Todas las habitaciones del hotel tienen aire acondicionado. ❄️🏨"],
+    ["I was at Sue’s (house) last night. 🏠🌙", "Estuve en la casa de Sue anoche. 🏠🌙"],
+    ["It’s always cold in Sue’s house. The heating doesn’t work very well. ❄️🏠", "Siempre hace frío en la casa de Sue. La calefacción no funciona muy bien. ❄️🏠"],
+    ["Sam’s parents live in St. Louis. 👨‍👩‍👧‍👦🏠", "Los padres de Sam viven en St. Louis. 👨‍👩‍👧‍👦🏠"],
+    ["The bus was very full. there were to many people on it. 🚌👥", "El autobús estaba muy lleno. Había demasiada gente en él. 🚌👥"],
+    ["Mary arrived in a taxi. 🚖👩", "Mary llegó en taxi. 🚖👩"],
+    ["Jane passed me on her bike. 🚴‍♀️👋", "Jane me adelantó en su bicicleta. 🚴‍♀️👋"],
+  ];  
+
+export default inAtOnPosition;
