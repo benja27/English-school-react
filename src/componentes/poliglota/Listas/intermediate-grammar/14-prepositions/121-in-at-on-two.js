8478c937bb0f4d3b27ba170fbf27a6a164633c54
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-06 18:28:36 -0600

    Content: add 121 In/at/on (Position) 2.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/121-in-at-on-two.js
@@ -0,0 +1,25 @@
+// 121 In/at/on (Position) 2
+
+const inAtOnTwo = [
+    ['When I go to the movies, I like to sit in the front row. 🎬👀', 'Cuando voy al cine, me gusta sentarme en la primera fila. 🎬👀'],
+    ['I just started working in the sales department. 💼👨‍💼', 'Acabo de empezar a trabajar en el departamento de ventas. 💼👨‍💼'],
+    ['Who is the woman in that photo? 📸👩', '¿Quién es la mujer en esa foto? 📸👩'],
+    ['Have you seen this picture in today’s paper? 📰👀', '¿Has visto esta imagen en el periódico de hoy? 📰👀'],
+    ['I was sitting in the back (of the car) when we crashed. 🚗👀', 'Estaba sentado en la parte de atrás (del coche) cuando chocamos. 🚗👀'],
+    ['Let’s sit in the front (of the movie theater). 🎥👫', 'Vamos a sentarnos al frente (del cine). 🎥👫'],
+    ['John was standing in the back of the crowd. 🚶‍♂️👥', 'John estaba parado en la parte de atrás de la multitud. 🚶‍♂️👥'],
+    ['In Britain they drive on the left. 🇬🇧🚗', 'En Gran Bretaña conducen por la izquierda. 🇬🇧🚗'],
+    ['Our apartment is on the second floor of the building. 🏢2️⃣', 'Nuestro apartamento está en el segundo piso del edificio. 🏢2️⃣'],
+    ['Here’s a shopping list. Don’t buy anything that’s not on the list. 🛒📝', 'Aquí tienes una lista de compras. No compres nada que no esté en la lista. 🛒📝'],
+    ['Have you ever worked on a farm? It’s a lot like working on a ranch. 🚜🐄', '¿Alguna vez has trabajado en una granja? Es muy parecido a trabajar en un rancho. 🚜🐄'],
+    ['Washington, D.C., is on the East Coast of the United States, on the Potomac River. 🇺🇸🌊', 'Washington, D.C., está en la costa este de los Estados Unidos, en el río Potomac. 🇺🇸🌊'],
+    ['I live on Main Street. My brother lives on Elm. 🏠🌳', 'Vivo en la calle Main. Mi hermano vive en Elm. 🏠🌳'],
+    ['We stopped at a small town on the way to Atlanta. 🚗🏞️', 'Paramos en un pequeño pueblo de camino a Atlanta. 🚗🏞️'],
+    ['I wrote the date on the back of the photo. 📅📸', 'Escribí la fecha en la parte posterior de la foto. 📅📸'],
+    ['Write your name at the top of the page. ✍️🔝', 'Escribe tu nombre en la parte superior de la página. ✍️🔝'],
+    ['Jane’s house is at the other end of the street. 🏠🛣️', 'La casa de Jane está al otro extremo de la calle. 🏠🛣️'],
+    ['The television is in the corner of the room. 📺🏠', 'El televisor está en la esquina de la habitación. 📺🏠'],
+    ['There is a mailbox at/on the corner of the street. 📬🛣️', 'Hay un buzón en la esquina de la calle. 📬🛣️'],
+  ];
+  
+export default inAtOnTwo;
