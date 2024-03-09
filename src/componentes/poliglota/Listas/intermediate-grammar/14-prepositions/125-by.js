4fe16bc3fa9019bb6625a3053bd0f4ee6414c8a0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-08 22:55:55 -0600

    Content: add 125 By.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/125-by.js
@@ -0,0 +1,23 @@
+// 125 By
+
+const by = [
+    ['Can I pay by credit card? 💳💸', '¿Puedo pagar con tarjeta de crédito? 💳💸'],
+    ['You can contact me by phone, by fax or by e-mail. 📞📠📧', 'Puedes contactarme por teléfono, por fax o por correo electrónico. 📞📠📧'],
+    ['We hadn’t arranged to meet. We met by chance. 🤝🍀', 'No habíamos quedado en encontrarnos. Nos encontramos por casualidad. 🤝🍀'],
+    ['I didn’t do it on purpose. It was an accident. 🤷‍♂️🚗', 'No lo hice a propósito. Fue un accidente. 🤷‍♂️🚗'],
+    ['Joanne usually goes to work by bus. 🚌👩‍💼', 'Joanne suele ir al trabajo en autobús. 🚌👩‍💼'],
+    ['Do you prefer to travel by plane or by train? ✈️🚆', '¿Prefieres viajar en avión o en tren? ✈️🚆'],
+    ['Did you come here by car or on foot? 🚗🚶‍♂️', '¿Viniste aquí en coche o a pie? 🚗🚶‍♂️'],
+    ['They didn’t come in their car. They came in a taxi. 🚖🚗', 'No vinieron en su coche. Vinieron en un taxi. 🚖🚗'],
+    ['We came on the 6:45 train. 🚂⏰', 'Vinimos en el tren de las 6:45. 🚂⏰'],
+    ['Have you ever been bitten by a dog? 🐕😬', '¿Alguna vez te ha mordido un perro? 🐕😬'],
+    ['The program was watched by millions of people. 📺👀', 'El programa fue visto por millones de personas. 📺👀'],
+    ['The door must have been opened with a key. 🚪🔑', 'La puerta debe haber sido abierta con una llave. 🚪🔑'],
+    ['The door must have been opened by somebody with a key. 🚪🔑🕵️‍♂️', 'La puerta debe haber sido abierta por alguien con una llave. 🚪🔑🕵️‍♂️'],
+    ['Have you read anything by Ernest Hemingway? 📚📖', '¿Has leído algo de Ernest Hemingway? 📚📖'],
+    ['Come and sit by me. 🪑👫', 'Ven y siéntate a mi lado. 🪑👫'],
+    ['Where’s the light switch? –By the door. 💡🚪', '¿Dónde está el interruptor de la luz? –Junto a la puerta. 💡🚪'],
+    ['Carl and Mike ran a 100 meter race. Carl won by about three meters. 🏃‍♂️🏅', 'Carl y Mike corrieron una carrera de 100 metros. Carl ganó por unos tres metros. 🏃‍♂️🏅'],
+  ];  
+
+export default by;
