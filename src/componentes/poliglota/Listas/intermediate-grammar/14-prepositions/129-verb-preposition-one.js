0a61e6c51072620ec553e3e17df189203df80255
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-12 20:27:42 -0600

    Content: Add 129 Verb + Perposition 1 to and at.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/129-verb-preposition-one.js
@@ -0,0 +1,24 @@
+// 129 Verb + Preposition 1  to and at
+
+const verbPrepositionOne = [
+    ['Who was that man you were talking to? 🤔🗣️', '¿Quién era ese hombre con el que estabas hablando? 🤔🗣️'],
+    ['We spent the evening listening to music. 🎶🌆', 'Pasamos la tarde escuchando música. 🎶🌆'],
+    ['They apologized to me for what happened. 🙏😔', 'Me pidieron disculpas por lo sucedido. 🙏😔'],
+    ['Can you explain this word to me? 📖🤔', '¿Puedes explicarme esta palabra? 📖🤔'],
+    ['I explained to them why I was worried. 🗣️😟', 'Les expliqué por qué estaba preocupado. 🗣️😟'],
+    ['Let me describe to you what I saw. 📝👀', 'Déjame describirte lo que vi. 📝👀'],
+    ['Did you call your father yesterday? 📞👨', '¿Llamaste a tu padre ayer? 📞👨'],
+    ['He refused to answer my question. 🚫❓', 'Se negó a responder mi pregunta. 🚫❓'],
+    ['Can I ask you a question? 🤔❓', '¿Puedo hacerte una pregunta? 🤔❓'],
+    ['He thanked me for helping him. 🙏🤝', 'Me dio las gracias por ayudarlo. 🙏🤝'],
+    ['Why are you looking at me like that? 🤨👀', '¿Por qué me estás mirando así? 🤨👀'],
+    ['I look stupid with this haircut. Everybody will laugh at me. 😞✂️', 'Me veo estúpido con este corte de pelo. Todos se reirán de mí. 😞✂️'],
+    ["Don’t point that knife at me. It’s dangerous. 🚫🔪", "No apuntes ese cuchillo hacia mí. Es peligroso. 🚫🔪"],
+    ["We saw someone with a gun shooting at birds, but he didn’t hit any. 🔫🐦", "Vimos a alguien con una pistola disparando a los pájaros, pero no le dio a ninguno. 🔫🐦"],
+    ["He got very angry and started shouting at me. 😡📢", "Se puso muy enojado y comenzó a gritarme. 😡📢"],
+    ["He shouted to me from the other side of the street. 📢🚶‍♂️", "Me gritó desde el otro lado de la calle. 📢🚶‍♂️"],
+    ["Somebody threw an egg at the politician. 🥚👔", "Alguien lanzó un huevo al político. 🥚👔"],
+    ["Lisa shouted “Catch!” and threw the keys to me from the window. 🗣️🔑", "Lisa gritó “¡Atrápalo!” y me lanzó las llaves desde la ventana. 🗣️🔑"],
+  ];  
+
+export default verbPrepositionOne;
