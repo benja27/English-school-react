9e00af44c924e5b6c8a6774cff08104cc79629f5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-13 19:37:20 -0600

    Content: Add 130 Verb + Preposition 2 about/for/of/after

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/130-verb-preposition-two.js
@@ -0,0 +1,25 @@
+// 130 Verb + Preposition 2 about/for/of/after
+
+const verbPrepositionTwo = [
+    ['We talked about a lot of things at the meeting. 💬👥', 'Hablamos de muchas cosas en la reunión. 💬👥'],
+    ['We had a discussion about what we should do. 💬🤔', 'Tuvimos una discusión sobre qué deberíamos hacer. 💬🤔'],
+    ['We discussed a lot of things at the meeting. 💬🗣️', 'Discutimos muchas cosas en la reunión. 💬🗣️'],
+    ['If you’re worried about the problem, you should do something about it. 😟🛠️', 'Si estás preocupado por el problema, deberías hacer algo al respecto. 😟🛠️'],
+    ['He’s very selfish. He doesn’t care about other people. 😒🚷', 'Es muy egoísta. No le importan las demás personas. 😒🚷'],
+    ['You can do what you like. I don’t care what you do. 🤷‍♂️🔄', 'Puedes hacer lo que quieras. No me importa lo que hagas. 🤷‍♂️🔄'],
+    ['Would you care for a cup of coffee? ☕😊', '¿Te gustaría tomar una taza de café? ☕😊'],
+    ['I don’t care for very hot weather. 🌞😒', 'No me gusta mucho el clima muy caluroso. 🌞😒'],
+    ['Alan is 85 and lives alone. He needs somebody to care for him. 👴❤️', 'Alan tiene 85 años y vive solo. Necesita a alguien que cuide de él. 👴❤️'],
+    ['John gave up his job to take care of his elderly parents. 👴👩‍🦳', 'John dejó su trabajo para cuidar a sus padres ancianos. 👴👩‍🦳'],
+    ['I’ll take care of all travel arrangements, you don’t need to do anything. ✈️📋', 'Me encargaré de todos los arreglos de viaje, no necesitas hacer nada. ✈️📋'],
+    ['I wrote to the company asking them for more information about the job. ✉️👨‍💼', 'Escribí a la empresa pidiéndoles más información sobre el trabajo. ✉️👨‍💼'],
+    ['I think you’d be good at this job. Why don’t you apply for it? 💼🤔', 'Creo que serías bueno en este trabajo. ¿Por qué no solicitas? 💼🤔'],
+    ['Don’t wait for me. I’ll join you later. ⏳👋', 'No me esperes. Me uniré más tarde. ⏳👋'],
+    ['I’m not going out yet. I’m waiting for the rain to stop. ☔⏳', 'Todavía no salgo. Estoy esperando a que pare de llover. ☔⏳'],
+    ['I’ve searched the house for my keys, but I still can’t find them. 🔍🔑', 'He buscado las llaves por toda la casa, pero aún no las encuentro. 🔍🔑'],
+    ['I haven’t seen her since she left (home) for the office this morning. 👀🕰️', 'No la he visto desde que salió de casa para la oficina esta mañana. 👀🕰️'],
+    ['I’ve lost my keys. Can you help me look for them? 🔑🔍', 'Perdí mis llaves. ¿Puedes ayudarme a buscarlas? 🔑🔍'],
+    ['You can borrow this book if you promise to look after it. 📚🤝', 'Puedes tomar prestado este libro si prometes cuidarlo. 📚🤝'],
+  ];  
+
+export default verbPrepositionTwo;
