61ec13b9deb69a9217f3a0e4c03d2b8dfbeb8b16
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-13 19:39:28 -0600

    Content: add 131 Verb + Perposition 3 about and of

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/131-verb-preposition-three.js
@@ -0,0 +1,27 @@
+// 131 Verb + Preposition 3 about and of
+
+const verbPrepositionThree = [
+    ['I dreamed about you last night. 💭😴', 'Soñé contigo anoche. 💭😴'],
+    ['Do you dream of/about being rich and famous? 💭💰', '¿Sueñas con ser rico y famoso? 💭💰'],
+    ['Don’t tell anyone what I said. –No, I wouldn’t dream of it. 🤫🚫', 'No le digas a nadie lo que dije. –No, ni lo sueñes. 🤫🚫'],
+    ['Did you hear about what happened at the club on Saturday night? 🗣️🌃', '¿Oíste lo que pasó en el club el sábado por la noche? 🗣️🌃'],
+    ['Who is Tom Hart? –I have no idea. I’ve never heard of him. 🤷‍♂️🤔', '¿Quién es Tom Hart? –No tengo ni idea. Nunca he oído hablar de él. 🤷‍♂️🤔'],
+    ['Have you heard from Jane recently? –Yes, she called a few days ago. 📞👂', '¿Has tenido noticias de Jane últimamente? –Sí, llamó hace unos días. 📞👂'],
+    ['I’ve thought about what you said, and I’ve decided to take your advice. 💭🤔', 'He pensado en lo que dijiste, y he decidido seguir tu consejo. 💭🤔'],
+    ['Will you lend me the money? –I’ll think about it. 💰🤔', '¿Me prestarás el dinero? –Lo pensaré. 💰🤔'],
+    ['He told me his name, but I can’t think of it now. 🤔📝', 'Me dijo su nombre, pero ahora no lo recuerdo. 🤔📝'],
+    ['That’s a good idea. Why didn’t I think of that? 💡🤷‍♂️', 'Esa es una buena idea. ¿Por qué no se me ocurrió a mí? 💡🤷‍♂️'],
+    ['What did you think of the film? –I didn’t think much of it(I didn’t like it much) 🎬🤔', '¿Qué te pareció la película? –No pensé mucho en ella (No me gustó mucho). 🎬🤔'],
+    ['When I’m alone, I often think of you. 💭❤️', 'Cuando estoy solo, a menudo pienso en ti. 💭❤️'],
+    ['My sister is thinking of/about going to Canada. 🇨🇦💭', 'Mi hermana está pensando en ir a Canadá. 🇨🇦💭'],
+    ['I’m glad you reminded me about the meeting. 📅😊', 'Me alegra que me hayas recordado la reunión. 📅😊'],
+    ['This house reminds me of the one I lived in when I was a child. 🏠👦', 'Esta casa me recuerda a la que vivía cuando era niño. 🏠👦'],
+    ['Look at this picture of Richard. who does he remind you of? 📷👤', 'Mira esta foto de Richard. ¿A quién te recuerda? 📷👤'],
+    ['We complained to the manager of the restaurant about the food. 🍽️😡', 'Nos quejamos con el gerente del restaurante sobre la comida. 🍽️😡'],
+    ['We called the doctor because George was complaining of a pain in his stomach. 📞👨‍⚕️', 'Llamamos al médico porque George se quejaba de un dolor en el estómago. 📞👨‍⚕️'],
+    ['I knew he was a strange person. I had been warned about him. 🤔⚠️', 'Sabía que era una persona extraña. Me habían advertido sobre él. 🤔⚠️'],
+    ['Vicky warned me about the traffic. She said it would be bad. 🚗⚠️', 'Vicky me advirtió sobre el tráfico. Dijo que sería malo. 🚗⚠️'],
+    ['Scientists have warned us about/off the effects of global warming. 🌍⚠️', 'Los científicos nos han advertido sobre los efectos del calentamiento global. 🌍⚠️'],
+  ];  
+
+export default verbPrepositionThree;
