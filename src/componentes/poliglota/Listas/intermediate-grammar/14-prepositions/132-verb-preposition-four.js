21b2e834fc293e82fa473efb2764cfed50bcafda
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-14 22:28:31 -0600

    Content: add 132 Verb + Preposition 4 of/for/from/on.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/14-prepositions/132-verb-preposition-four.js
@@ -0,0 +1,25 @@
+// 132 Verb + Preposition 4 of/for/from/on
+
+const verbPrepositionFour = [
+    ['Sue accused me of being selfish. 😠👩‍⚖️', 'Sue me acusó de ser egoísta. 😠👩‍⚖️'],
+    ['Some students were suspected of cheating on the exam. 📝🕵️‍♂️', 'Algunos estudiantes fueron sospechosos de hacer trampa en el examen. 📝🕵️‍♂️'],
+    ['His parents don’t approve of what he does, but they can’t stop him. 👨‍👩‍👦‍👦🚫', 'Sus padres no aprueban lo que hace, pero no pueden detenerlo. 👨‍👩‍👦‍👦🚫'],
+    ['What did he die of? –A heart attack. 💔⚰️', '¿De qué murió? –De un ataque al corazón. 💔⚰️'],
+    ['We had an enormous meal. It consisted of seven courses. 🍽️👌', 'Tuvimos una comida enorme. Consistió en siete platos. 🍽️👌'],
+    ['I didn’t have enough money to pay for the meal. 💸🙅‍♂️', 'No tenía suficiente dinero para pagar la comida. 💸🙅‍♂️'],
+    ['I didn’t have enough money to pay the rent. 💸🏠', 'No tenía suficiente dinero para pagar el alquiler. 💸🏠'],
+    ['I’ll never forgive them for what they did. 😠🙅‍♂️', 'Nunca les perdonaré por lo que hicieron. 😠🙅‍♂️'],
+    ['When I realized I was wrong, I apologize (to them) for my mistake. 😞🙏', 'Cuando me di cuenta de que estaba equivocado, me disculpé (con ellos) por mi error. 😞🙏'],
+    ['Everybody blamed me for the accident. 😞🚗', 'Todos me culparon por el accidente. 😞🚗'],
+    ['Everybody said that I was to blame for the accident. 😞🚗', 'Todos dijeron que yo era el culpable del accidente. 😞🚗'],
+    ['Everybody blamed the accident on me. 😞🚗', 'Todos pusieron la culpa del accidente en mí. 😞🚗'],
+    ['The number of people suffering form heart disease has increased. 💔📈', 'El número de personas que sufren enfermedades cardíacas ha aumentado. 💔📈'],
+    ['Sunblock protects the skin from the sun. ☀️🛡️', 'El bloqueador solar protege la piel del sol. ☀️🛡️'],
+    ['What time will you be home? I don’t know. It depends on the traffic. 🕒🚗', '¿A qué hora llegarás a casa? No sé. Depende del tráfico. 🕒🚗'],
+    ['You can really on Jill. She always keeps her promises. 💪💬', 'Puedes confiar en Jill. Siempre cumple sus promesas. 💪💬'],
+    ['Are you going to buy it? –It depends how much is it.. 🤔💰', '¿Vas a comprarlo? –Depende de cuánto cueste.. 🤔💰'],
+    ['Michael’s salary is very low. It isn’t enough to live on. 💰😔', 'El salario de Michael es muy bajo. No es suficiente para vivir. 💰😔'],
+    ['I congratulated her on being admitted to law school. 🎓👏', 'La felicité por ser admitida en la escuela de leyes. 🎓👏'],
+  ];  
+
+export default verbPrepositionFour;
