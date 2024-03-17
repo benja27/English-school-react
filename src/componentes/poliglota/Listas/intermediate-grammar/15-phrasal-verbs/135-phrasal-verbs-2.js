0468e3d5d843c904d3de436e0597fa42160b45be
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-16 20:49:55 -0600

    Content: add 135 Phrasal Verbs 2 in/out.

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/15-phrasal-verbs/135-phrasal-verbs-2.js
@@ -0,0 +1,31 @@
+// 135 Phrasal Verbs 2 in/out
+
+const phrasalVerbsTwo = [
+    ['How did the thieves get in? 🔑🚪', '¿Cómo entraron los ladrones? 🔑🚪'],
+    ['Here’s a key, so you can let yourself in. 🔑🏠', 'Aquí tienes una llave, así puedes dejarte entrar. 🔑🏠'],
+    ['Sally walked up to the edge of the pool and dived in. 🏊‍♀️💦', 'Sally caminó hasta el borde de la piscina y se zambulló. 🏊‍♀️💦'],
+    ['I’ve got a new apartment. I’m moving in on Friday. 🏠📦', 'Tengo un nuevo apartamento. Me mudo el viernes. 🏠📦'],
+    ['As soon as I got to the airport, I checked in. ✈️👜', 'Tan pronto como llegué al aeropuerto, hice el check-in. ✈️👜'],
+    ['I’m moving in next week. 📦🗓️', 'Me mudo la próxima semana. 📦🗓️'],
+    ['I’m moving into my new apartment on Friday. 🏠🚚', 'Me mudo a mi nuevo apartamento el viernes. 🏠🚚'],
+    ['He just stood up and walked out. 🚶‍♂️🚪', 'Él simplemente se levantó y se fue. 🚶‍♂️🚪'],
+    ['I had no key, so I was locked out. 🔒🚪', 'No tenía llave, así que me quedé afuera. 🔒🚪'],
+    ['She swam up and down the pool, and then climbed out. 🏊‍♀️🔄', 'Ella nadó de arriba abajo en la piscina, y luego salió. 🏊‍♀️🔄'],
+    ['Tim opened the window and looked out. 👀🪟', 'Tim abrió la ventana y miró afuera. 👀🪟'],
+    ['What time doe we have to check out? ⏰🛏️', '¿A qué hora tenemos que hacer el check-out? ⏰🛏️'],
+    ['He walked out. 🚶‍♂️🚪', 'Se fue. 🚶‍♂️🚪'],
+    ['He walked out of the room. 🚶‍♂️🚪', 'Se salió de la habitación. 🚶‍♂️🚪'],
+    ['I dropped in to see Chris on my way home. 🏡👋', 'Pasé a ver a Chris de camino a casa. 🏡👋'],
+    ['We’re playing a game. Why don’t you join in? 🎲🤔', 'Estamos jugando un juego. ¿Por qué no te unes? 🎲🤔'],
+    ['The fridge isn’t working because you haven’t plugged it in. ❄️🔌', 'La nevera no está funcionando porque no la has enchufado. ❄️🔌'],
+    ['Your report is due this week. Please hand it in by Friday at 3 p.m. 📄⏰', 'Tu informe vence esta semana. Por favor entrégalo antes del viernes a las 3 p.m. 📄⏰'],
+    ['Some children have trouble fitting in at a new school. 🏫👧👦', 'Algunos niños tienen problemas para adaptarse a una nueva escuela. 🏫👧👦'],
+    ["There wasn’t anything to eat at home, so we decided to eat out. 🍽️🏠", "No había nada que comer en casa, así que decidimos comer fuera. 🍽️🏠"],
+    ['Gary went to college but dropped out after a year. 🎓❌', 'Gary fue a la universidad pero abandonó después de un año. 🎓❌'],
+    ["I promised I’d go to the wedding. I don’t want to go, but I can’t get out of it now. 💒😕", "Prometí que iría a la boda. No quiero ir, pero ya no puedo evitarlo. 💒😕"],
+    ['There was a beautiful picture in the magazine, so I cut it out and kept it. 📰✂️', 'Había una hermosa imagen en la revista, así que la recorté y la guardé. 📰✂️'],
+    ['In the sentence “She said that she was sick,” you can leave out  the word “that.” 📝👩‍⚖️', 'En la oración “Ella dijo que estaba enferma,” puedes dejar fuera la palabra “que.” 📝👩‍⚖️'],
+    ['I have to fill out this application by the end of the week. 📝🗓️', 'Tengo que completar esta solicitud para el fin de semana. 📝🗓️'],
+  ];  
+
+export default phrasalVerbsTwo;
