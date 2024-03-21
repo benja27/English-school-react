8143a645fd86aae3756b3c3ca4fe2425ec81098d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-20 22:08:52 -0600

    Content: add 137 Phrasal Verbs 4 on/off(1).

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/15-phrasal-verbs/137-phrasal-verbs.4.js
@@ -0,0 +1,24 @@
+// 137 Phrasal Verbs 4 on/off(1)
+
+const phrasalVerbsFour = [
+    ['Should I leave the lights on or turn them off? 💡❓', '¿Debería dejar las luces encendidas o apagarlas? 💡❓'],
+    ['Is the heat on? –No, I shut it off. 🌡️🔥', '¿Está encendida la calefacción? –No, la apagué. 🌡️🔥'],
+    ['Who left the computer on? 💻🤔', '¿Quién dejó la computadora encendida? 💻🤔'],
+    ['What’s this CD like? –It’s great. Should I put it on? 🎶👍', '¿Cómo es este CD? –Es genial. ¿Debería ponerlo? 🎶👍'],
+    ['What’s all that noise? What’s going on? 🤔🔊', '¿Qué es todo ese ruido? ¿Qué está pasando? 🤔🔊'],
+    ['The open air concert had to be called off because of the weather. 🎤🚫', 'El concierto al aire libre tuvo que ser cancelado debido al clima. 🎤🚫'],
+    ['The wedding has been put off until January. 💒📅', 'La boda ha sido pospuesta hasta enero. 💒📅'],
+    ['We can’t put off making a decision. We have to decide now. 🤷‍♂️⏳', 'No podemos posponer tomar una decisión. Debemos decidir ahora. 🤷‍♂️⏳'],
+    ['My hands were cold, so I put my gloves on. 🧤❄️', 'Mis manos estaban frías, así que me puse los guantes. 🧤❄️'],
+    ['I’ve put on five pounds in the last month. 🍔⚖️', 'He aumentado cinco libras en el último mes. 🍔⚖️'],
+    ['I tried on a jacket in the store, but it didn’t fit me very well. 👕🛍️', 'Probé una chaqueta en la tienda, pero no me quedaba muy bien. 👕🛍️'],
+    ['I like the perfume you had on yesterday. 👃🌸', 'Me gusta el perfume que tenías puesto ayer. 👃🌸'],
+    ['It was warm, so I took off my jacket. ☀️🧥', 'Hacía calor, así que me quité la chaqueta. ☀️🧥'],
+    ['Tomorrow I’m off to Paris. 🗼🇫🇷', 'Mañana me voy a París. 🗼🇫🇷'],
+    ['Diane got on her bike and rode off. 🚲👋', 'Diane se subió a su bicicleta y se fue. 🚲👋'],
+    ['Mark left home at the age of 18 and went off to Canada. 🏠🇨🇦', 'Mark dejó su casa a los 18 años y se fue a Canadá. 🏠🇨🇦'],
+    ['After a long delay the plane finally took off. ✈️🛫', 'Después de un largo retraso, finalmente despegó el avión. ✈️🛫'],
+    ['Helen was going away. We went to the station with her to see her off. 🚉👋', 'Helen se iba. Fuimos a la estación con ella para despedirla. 🚉👋'],
+  ];  
+
+export default phrasalVerbsFour;
