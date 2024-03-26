180caacbd310810a61e8614a683edc6e0bf06eab
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-25 22:20:26 -0600

    Content: add 140 Phrasal Verbs 7 up(1).

---
+++ src/componentes/poliglota/Listas/intermediate-grammar/15-phrasal-verbs/140-phrasal-verbs-7.js
@@ -0,0 +1,27 @@
+// 140 Phrasal Verbs 7 up (1)
+
+const phrasalVerbsSeven = [
+    ['A man came up to me in the street and asked me for money. 👨‍💼💰', 'Un hombre se me acercó en la calle y me pidió dinero. 👨‍💼💰'],
+    ['I’m not ready to go yet. You go on and I’ll catch up with you. 🚶‍♂️⏳', 'Todavía no estoy listo para irme. Tú sigue adelante y yo te alcanzo. 🚶‍♂️⏳'],
+    ["You’re walking too fast. I can’t keep up (with you). 🚶‍♂️⏩", 'Estás caminando demasiado rápido. No puedo seguirte el ritmo. 🚶‍♂️⏩'],
+    ['You’re doing well. Keep it up! 👍💪', '¡Lo estás haciendo bien! ¡Sigue así! 👍💪'],
+    ['The government has set up a committee to investigate the problem. 🏛️🔍', 'El gobierno ha establecido un comité para investigar el problema. 🏛️🔍'],
+    ['Laura took up photography a few years ago. She takes really good pictures. 📷🌟', 'Laura comenzó a practicar fotografía hace unos años. Ella toma fotos realmente buenas. 📷🌟'],
+    ['Sara was born in Mexico but grew up in the United States. 🇲🇽👧🇺🇸', 'Sara nació en México pero creció en los Estados Unidos. 🇲🇽👧🇺🇸'],
+    ['Her parents died when she was a child, so she was brought up by her grandparents. 👴👵👧', 'Sus padres murieron cuando era niña, así que la criaron sus abuelos. 👴👵👧'],
+    ['Will you back me up if I tell the police what happened? (= say I’m telling the truth). 🚔👍', '¿Me respaldarás si le digo a la policía lo que sucedió? (= decir que estoy diciendo la verdad). 🚔👍'],
+    ["You’ve spent a long time on that document; you’d better back up your files. 💻💾", 'Has pasado mucho tiempo en ese documento; más vale que hagas una copia de seguridad de tus archivos. 💻💾'],
+    ["I couldn’t turn around in the narrow street. I had to back the car up for a block. 🚗↩️", 'No pude dar la vuelta en la calle estrecha. Tuve que retroceder el coche durante una cuadra. 🚗↩️'],
+    ['Cars are back up for a mile at the entrance to the stadium. 🚗🚧', 'Los autos están retrocediendo una milla en la entrada del estadio. 🚗🚧'],
+    ['There was a fight in the street and three men ended up in the hospital. 🥊🏥', 'Hubo una pelea en la calle y tres hombres terminaron en el hospital. 🥊🏥'],
+    ["I couldn’t find a hotel and ended up sleeping on a bench at the station. 🏨😴", 'No pude encontrar un hotel y terminé durmiendo en un banco en la estación. 🏨😴'],
+    ['Don’t give up. Keep trying! 🚫🆙', '¡No te rindas! ¡Sigue intentándolo! 🚫🆙'],
+    ['Ted failed his driving test at age 80, so he had to give up driving. 🚗❌', 'Ted reprobó su examen de manejo a los 80 años, así que tuvo que dejar de conducir. 🚗❌'],
+    ['Children under 16 make up half the population of the city. 👦👧📊', 'Los niños menores de 16 años representan la mitad de la población de la ciudad. 👦👧📊'],
+    ['Air is made up mainly of nitrogen and oxygen. 💨🧪', 'El aire está compuesto principalmente de nitrógeno y oxígeno. 💨🧪'],
+    ['Most of the space in the room was taken up by a large table. 🏠🪑', 'La mayor parte del espacio en la habitación estaba ocupado por una mesa grande. 🏠🪑'],
+    ["We arranged to meet Dave last night, but he didn’t turn up. 🤷‍♂️🕒", 'Quedamos en encontrarnos con Dave anoche, pero no apareció. 🤷‍♂️🕒'],
+    ['I’m going to make some soup. I want to use up the vegetables I have. 🥣🥕', 'Voy a hacer sopa. Quiero usar las verduras que tengo. 🥣🥕'],
+  ];  
+
+export default phrasalVerbsSeven;
